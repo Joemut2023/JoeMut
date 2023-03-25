0d49680afeacce1defb251433ca6196d1ab395b7
seleshabani <seleshabani4@gmail.com>
2023-03-25 18:24:13 +0100

    migrations/panier : configuration de la db

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ var logger = require('morgan');
 const ejsLayout = require('express-ejs-layouts');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
-
+require("./config/db").sync();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
