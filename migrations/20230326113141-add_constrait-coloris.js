27e275a5b846dfb61f3b2090eefd0096eefb46d9
pathymavuba <mavubapathy@gmail.com>
2023-03-26 12:38:19 +0100

    migration/produit:relation de couleur et coloris est établie

---
+++ migrations/20230326113141-add_constrait-coloris.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addConstraint('Coloris',{
+      fields:["cou_id"],
+      type:"foreign key",
+      name:"coud_id_in_coloris",
+      references:{
+        table:"Couleurs",
+        field:"cou_id"
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
