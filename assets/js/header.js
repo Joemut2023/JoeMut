79a1bd3e6aa0f41d826cbc97e8b6ca71849aa2ac
seleshabani <seleshabani4@gmail.com>
2023-03-31 14:44:16 +0100

    migration/temoignage : test with victoire

--- assets/js/header.js
+++ assets/js/header.js
@@ -13,6 +13,7 @@ const handler = (entries) => {
   }
 };
 
+
 // create the observer
 const observer = new window.IntersectionObserver(handler);
 // give the observer some dom nodes to keep an eye on
