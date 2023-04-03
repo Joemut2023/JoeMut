d164bb8ee817ad5fe8b5723ab5a5fa974fe34838
pathymavuba <mavubapathy@gmail.com>
2023-04-03 19:23:12 +0100

    develop:correct conflict in mais.css

---
+++ public/javascripts/modal.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var myModal = document.getElementById('myModal');
+var myInput = document.getElementById('myInput');
+myModal.addEventListener('shown.bs.modal', function () {
+  //   myInput.focus()
+  alert("modal");
+});
