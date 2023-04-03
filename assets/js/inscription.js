3d2f0cc4e372cf2fe9540eb0cb41cba52cf750c5
Trigo YODILA <trigoyodila1996@gmail.com>
2023-04-03 12:36:01 +0100

    feature/inscription : ajout action cacher et montrer mot de passe

--- assets/js/inscription.js
+++ assets/js/inscription.js
@@ -1,1 +1,14 @@
-console.log("salut");
+const password = document.getElementById("password");
+const btn = document.getElementById("btn-action");
+
+btn.addEventListener("click", function(e){
+    e.preventDefault()
+
+     const type = password.getAttribute("type") === "password" ? "text" : "password";
+     password.setAttribute("type", type);
+     
+     if (password.getAttribute("type") === "password") 
+        btn.textContent = "Montrer"
+     else
+        btn.textContent = "Cacher"
+})
