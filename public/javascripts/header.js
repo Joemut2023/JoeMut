0c5d8f88d0928baa909760606fa9b835a0e21efd
Romain kabasi <kabasiromain@gmail.com>
2023-04-03 15:49:45 +0100

    fix/header

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -3,7 +3,8 @@
 var topBar = document.querySelector(".top-bar");
 var sentinal = document.querySelector(".sentinal");
 var headerSticky = document.querySelector("#headerSticky");
-console.log(headerSticky);
+var userIcone = document.querySelector("#user-title-Dropdown");
+var dropDown = document.querySelector("#user-menu-Dropdown");
 var handler = function handler(entries) {
   if (!entries[0].isIntersecting) {
     headerSticky.style.display = "block";
@@ -18,3 +19,19 @@ var handler = function handler(entries) {
 var observer = new window.IntersectionObserver(handler);
 // give the observer some dom nodes to keep an eye on
 observer.observe(topBar);
+userIcone.addEventListener("mouseenter", function (event) {
+  dropDown.style.display = "block";
+  dropDown.style.opacity = "1";
+  dropDown.style.transform = "scaleY(1)";
+  dropDown.style.transformOrigin = "top";
+}
+// highlight the mouseenter target
+);
+
+dropDown.addEventListener("mouseout", function (event) {
+  // dropDown.style.opacity = "0";
+  // dropDown.style.transform="scaleY(0)";
+  dropDown.style.display = "none";
+}
+// highlight the mouseenter target
+);
