b7cf9a6b149c6640ae7f6896464c4c3bcfacf58f
vict97 <myindavictoire@gmail.com>
2023-04-04 14:23:05 +0100

    develop/victoire : ajout de la fonctionalité js dans commande

--- assets/js/commander.js
+++ assets/js/commander.js
@@ -1,14 +1,69 @@
 const password = document.getElementById("password");
 const btn = document.getElementById("btn-action");
+const infoForm = document.querySelector('#info-form')
+const adresseForm = document.querySelector('#adresse-form')
+const livraisonForm = document.querySelector('#livraison-form')
+const infoContent = document.querySelector('#info-content')
+const adresseDiv = document.querySelector('#divAdress')
+const livraisonDiv = document.querySelector('#divLivraison')
+const finalisationDiv = document.querySelector('#divFinalisation')
+const adresseContainer = document.querySelector('#adresse')
+const livraisonContainer = document.querySelector('#livraison')
+const finalisationContainer = document.querySelector('#finalisation')
+const btnModiferInfo = document.querySelector('#info-mod-btn')
+const btnModiferAdresse = document.querySelector('#adresse-mod-btn')
+const btnModiferlivraison = document.querySelector('#livraison-mod-btn')
+const btnModiferfinalisation = document.querySelector('#finalisation-mod-btn')
+    // const btnModiferInfo = document.querySelector('#info-mod-btn')
+const faCheck = document.querySelector('#faCheck')
 
-btn.addEventListener("click", function(e) {
-    e.preventDefault()
 
-    const type = password.getAttribute("type") === "password" ? "text" : "password";
-    password.setAttribute("type", type);
+const eventAccordeonForm = (nextDiv, currentDiv, btnModifier, nexDiv, ) => {
+    nextDiv.style.display = "block"
+    currentDiv.style.display = "none"
+    btnModifier.style.display = "block"
+    nexDiv.style.opacity = "1"
+}
 
-    if (password.getAttribute("type") === "password")
-        btn.textContent = "Montrer"
-    else
-        btn.textContent = "Cacher"
+infoForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    eventAccordeonForm(adresseDiv, infoContent, btnModiferInfo, adresseContainer)
 })
+btnModiferInfo.addEventListener("click", (event) => {
+    event.preventDefault()
+    adresseDiv.style.display = "none"
+    infoContent.style.display = "flex"
+    adresseTitle.style.pointerEvents = "none"
+    adresseTitle.style.opacity = "0.6"
+})
+adresseForm.addEventListener("submit", (event) => {
+        event.preventDefault()
+        eventAccordeonForm(livraisonDiv, adresseDiv, btnModiferAdresse, livraisonContainer)
+    }) // pointer-events: none;
+
+livraisonForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    eventAccordeonForm(finalisationDiv, livraisonDiv, btnModiferlivraison, finalisationContainer)
+})
+
+
+// btn.addEventListener("click", function(e) {
+//     e.preventDefault()
+
+//     const type = password.getAttribute("type") === "password" ? "text" : "password";
+//     password.setAttribute("type", type);
+
+//     if (password.getAttribute("type") === "password")
+//         btn.textContent = "Montrer"
+//     else
+//         btn.textContent = "Cacher"
+// })
+
+// function toggle_visibility(id) {
+//     var element = document.getElementById(id);
+//     if (element.style.display === "none") {
+//         element.style.display = "block";
+//     } else {
+//         element.style.display = "none";
+//     }
+// }
--- public/javascripts/commander.js
+++ public/javascripts/commander.js
@@ -2,9 +2,66 @@
 
 var password = document.getElementById("password");
 var btn = document.getElementById("btn-action");
-btn.addEventListener("click", function (e) {
-  e.preventDefault();
-  var type = password.getAttribute("type") === "password" ? "text" : "password";
-  password.setAttribute("type", type);
-  if (password.getAttribute("type") === "password") btn.textContent = "Montrer";else btn.textContent = "Cacher";
+var infoForm = document.querySelector('#info-form');
+var adresseForm = document.querySelector('#adresse-form');
+var livraisonForm = document.querySelector('#livraison-form');
+var infoContent = document.querySelector('#info-content');
+var adresseDiv = document.querySelector('#divAdress');
+var livraisonDiv = document.querySelector('#divLivraison');
+var finalisationDiv = document.querySelector('#divFinalisation');
+var adresseContainer = document.querySelector('#adresse');
+var livraisonContainer = document.querySelector('#livraison');
+var finalisationContainer = document.querySelector('#finalisation');
+var btnModiferInfo = document.querySelector('#info-mod-btn');
+var btnModiferAdresse = document.querySelector('#adresse-mod-btn');
+var btnModiferlivraison = document.querySelector('#livraison-mod-btn');
+var btnModiferfinalisation = document.querySelector('#finalisation-mod-btn');
+// const btnModiferInfo = document.querySelector('#info-mod-btn')
+var faCheck = document.querySelector('#faCheck');
+var eventAccordeonForm = function eventAccordeonForm(nextDiv, currentDiv, btnModifier, nexDiv) {
+  nextDiv.style.display = "block";
+  currentDiv.style.display = "none";
+  btnModifier.style.display = "block";
+  nexDiv.style.opacity = "1";
+};
+infoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  eventAccordeonForm(adresseDiv, infoContent, btnModiferInfo, adresseContainer);
 });
+btnModiferInfo.addEventListener("click", function (event) {
+  event.preventDefault();
+  adresseDiv.style.display = "none";
+  infoContent.style.display = "flex";
+  adresseTitle.style.pointerEvents = "none";
+  adresseTitle.style.opacity = "0.6";
+});
+adresseForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  eventAccordeonForm(livraisonDiv, adresseDiv, btnModiferAdresse, livraisonContainer);
+}); // pointer-events: none;
+
+livraisonForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  eventAccordeonForm(finalisationDiv, livraisonDiv, btnModiferlivraison, finalisationContainer);
+});
+
+// btn.addEventListener("click", function(e) {
+//     e.preventDefault()
+
+//     const type = password.getAttribute("type") === "password" ? "text" : "password";
+//     password.setAttribute("type", type);
+
+//     if (password.getAttribute("type") === "password")
+//         btn.textContent = "Montrer"
+//     else
+//         btn.textContent = "Cacher"
+// })
+
+// function toggle_visibility(id) {
+//     var element = document.getElementById(id);
+//     if (element.style.display === "none") {
+//         element.style.display = "block";
+//     } else {
+//         element.style.display = "none";
+//     }
+// }
