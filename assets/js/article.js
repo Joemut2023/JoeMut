8e142b44539a231dbdce7105e1b1ff348e20fb4a
Trigo YODILA <trigoyodila1996@gmail.com>
2023-04-04 21:25:07 +0100

    feature/article : changement d'images dans le modal lors du clic

--- assets/js/article.js
+++ assets/js/article.js
@@ -1,6 +1,14 @@
 const navlink_one = document.querySelector(".btn-tab-one");
 const navlink_two = document.querySelector(".btn-tab-two");
 
+const btn_up = document.querySelector(".btn-up");
+const btn_down = document.querySelector(".btn-down");
+const number = parseInt(document.querySelector(".number-value").value);
+const input = document.querySelector(".number-value");
+
+const image_grand = document.querySelector(".img-grand");
+const image_small = document.querySelectorAll(".img-small");
+
 navlink_one.addEventListener("click", function () {
   navlink_one.classList.add("button-add");
   navlink_two.classList.remove("button-add");
@@ -10,3 +18,20 @@ navlink_two.addEventListener("click", function () {
   navlink_two.classList.add("button-add");
   navlink_one.classList.remove("button-add");
 });
+
+// Incrementation
+btn_up.addEventListener("click", function () {
+  number = parseInt(isNaN(number)) ? 1 : parseInt(number);
+  number++;
+  input.value = number
+  
+});
+
+// Images
+image_small.forEach(element => {
+  element.addEventListener("click", function () {
+    // alert("click yes !")
+    image_grand.src = element.src
+  });
+});
+
--- public/javascripts/article.js
+++ public/javascripts/article.js
@@ -1,7 +1,14 @@
 "use strict";
 
+function _readOnlyError(name) { throw new TypeError("\"" + name + "\" is read-only"); }
 var navlink_one = document.querySelector(".btn-tab-one");
 var navlink_two = document.querySelector(".btn-tab-two");
+var btn_up = document.querySelector(".btn-up");
+var btn_down = document.querySelector(".btn-down");
+var number = parseInt(document.querySelector(".number-value").value);
+var input = document.querySelector(".number-value");
+var image_grand = document.querySelector(".img-grand");
+var image_small = document.querySelectorAll(".img-small");
 navlink_one.addEventListener("click", function () {
   navlink_one.classList.add("button-add");
   navlink_two.classList.remove("button-add");
@@ -9,4 +16,19 @@ navlink_one.addEventListener("click", function () {
 navlink_two.addEventListener("click", function () {
   navlink_two.classList.add("button-add");
   navlink_one.classList.remove("button-add");
+});
+
+// Incrementation
+btn_up.addEventListener("click", function () {
+  parseInt(isNaN(number)) ? 1 : parseInt(number), _readOnlyError("number");
+  +number, _readOnlyError("number");
+  input.value = number;
+});
+
+// Images
+image_small.forEach(function (element) {
+  element.addEventListener("click", function () {
+    // alert("click yes !")
+    image_grand.src = element.src;
+  });
 });
