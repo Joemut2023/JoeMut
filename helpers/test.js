1fb91eadf56fbc43455ba8332b78474d479d5d7a
pathymavuba <mavubapathy@gmail.com>
2023-04-05 15:31:49 +0100

    feature/seeder:"correct data in Titres

--- helpers/test.js
+++ helpers/test.js
@@ -1,7 +1,9 @@
+
 const fs = require("fs");
 const csv = require("csv-parser");
 const path = require('path');
-let results = [];
+var results = [];
+
 fs.createReadStream(path.resolve(__dirname, '../datas/customers.csv'))
 .pipe(csv({separator:"\n",headers:['titre','prenom','nom','email']}))
 .on("data", (data) => { results.push(data)})
@@ -9,7 +11,6 @@ fs.createReadStream(path.resolve(__dirname, '../datas/customers.csv'))
     results.map(element => {
         console.log(element.titre.split(";"));
     })
+  
 });
 // .on('end',()=>{console.log(results[1].titre.split(';'));});
-
-module.exports = results
