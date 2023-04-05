272b593f42a0a201c2984cc9ceb50816e1389285
pathymavuba <mavubapathy@gmail.com>
2023-04-05 15:04:10 +0100

    feature/seeder:remove column cli_login in clients and create seed for titres

--- helpers/test.js
+++ helpers/test.js
@@ -5,4 +5,11 @@ let results = [];
 fs.createReadStream(path.resolve(__dirname, '../datas/customers.csv'))
 .pipe(csv({separator:"\n",headers:['titre','prenom','nom','email']}))
 .on("data", (data) => { results.push(data)})
-.on('end',()=>{console.log(results[1].titre.split(';'));});
+.on('end',()=>{
+    results.map(element => {
+        console.log(element.titre.split(";"));
+    })
+});
+// .on('end',()=>{console.log(results[1].titre.split(';'));});
+
+module.exports = results
