d727ea0f1defc711706fce57591a8a4d4c5b22bc
pathymavuba <mavubapathy@gmail.com>
2023-04-07 18:36:12 +0100

    feature/typeCatalogue:affichage de produits par type des catégories

--- routes/catalogue.js
+++ routes/catalogue.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { Categorie } = require("../models");
+const { Categorie,Type_categorie} = require("../models");
 
 /* GET home page. */
 router.get('/',async function(req, res, next) {
@@ -30,6 +30,28 @@ router.get('/:id',async (req,res)=>{
      });
   }
   
+})
+
+router.get('/type/:id',async (req,res)=>{
+  const id = req.params.id;
+   try {
+    const type_categorie = await Type_categorie.findByPk(id,{include:{model:Categorie}});
+     const categories = await Categorie.findAll();
+    //  const categorie = await Categorie.findByPk(id);
+     res.locals.titre = type_categorie.tyc_libelle;
+    //  res.json({type_categorie})
+     return res.render("catalogue/bytype", {
+       title: "Express",
+       categories: categories,
+      //  categorie: categorie,
+      type_categorie :type_categorie
+     });
+   } catch (error) {
+     res.status(500).render("inscription/index", {
+       error: true,
+       errorMsg: "Une erreur est survenue!",
+     });
+   }
 })
 
 module.exports = router;
