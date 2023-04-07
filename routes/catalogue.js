d7dd4a31a6051a281b06cfaf0ae5c60294a7c45b
pathymavuba <mavubapathy@gmail.com>
2023-04-07 15:24:15 +0100

    feature/catalogueDynamic:dynamisation de types de catégories sur la page catalogue

--- routes/catalogue.js
+++ routes/catalogue.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const { Categorie } = require("../models");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('catalogue/index', { title: 'Express' });
+router.get('/',async function(req, res, next) {
+  try {
+    const categories = await Categorie.findAll()
+    return res.render("catalogue/index", { title: "Express" , categories: categories});
+    
+  } catch (error) {
+     res.status(500).render("inscription/index", {
+       error: true,
+       errorMsg: "Une erreur est survenue!",
+     });
+  }
+  
 });
 
 module.exports = router;
