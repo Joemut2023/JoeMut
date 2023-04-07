b640e687f1aa98ef55e4d84c0ef96bdf567e27f1
Romain kabasi <kabasiromain@gmail.com>
2023-04-07 12:55:06 +0100

    fix/link : add all missed links

--- public/javascripts/modal.js
+++ public/javascripts/modal.js
@@ -3,11 +3,17 @@
 var myModal = document.getElementById("myModal");
 var close = document.getElementById("close");
 var btn_add = document.querySelectorAll(".button-ajouter");
+var btn_continuer = document.querySelectorAll(".continuer");
 close.addEventListener("click", function () {
   myModal.style.display = "none";
 });
 btn_add.forEach(function (element) {
   element.addEventListener("click", function () {
     myModal.style.display = "flex";
+  });
+});
+btn_continuer.forEach(function (element) {
+  element.addEventListener("click", function () {
+    myModal.style.display = "none";
   });
 });
