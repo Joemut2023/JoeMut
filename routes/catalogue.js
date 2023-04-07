55c2bab53de954415ad0fba3b48323471bbb83be
pathymavuba <mavubapathy@gmail.com>
2023-04-07 18:57:02 +0100

    feature/typeCatalogue:add link

--- routes/catalogue.js
+++ routes/catalogue.js
@@ -5,8 +5,13 @@ const { Categorie,Type_categorie} = require("../models");
 /* GET home page. */
 router.get('/',async function(req, res, next) {
   try {
+    const typee_categories = await Type_categorie.findAll()
     const categories = await Categorie.findAll()
-    return res.render("catalogue/index", { title: "Express" , categories: categories});
+    return res.render("catalogue/index", {
+      title: "Express",
+      categories: categories,
+      typee_categories: typee_categories
+    });
     
   } catch (error) {
      res.status(500).render("inscription/index", {
@@ -19,10 +24,16 @@ router.get('/',async function(req, res, next) {
 router.get('/:id',async (req,res)=>{
   const id = req.params.id;
   try {
+    const typee_categories = await Type_categorie.findAll();
     const categories = await Categorie.findAll()
     const categorie = await Categorie.findByPk(id);
     res.locals.titre = categorie.cat_libelle
-    return res.render("catalogue/bycategorie", { title: "Express" , categories: categories,categorie:categorie});
+    return res.render("catalogue/bycategorie", {
+      title: "Express",
+      categories: categories,
+      categorie: categorie,
+      typee_categories: typee_categories,
+    });
   } catch (error) {
      res.status(500).render("inscription/index", {
        error: true,
