e8e7b1b55e06d3ec185c07985213529eadd5688d
pathymavuba <mavubapathy@gmail.com>
2023-04-08 20:51:48 +0100

    seedr/typeMedia:remplissage de la table type-media

---
+++ seeders/20230408192823-insert_into_typeMedia.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert("Type_medias", [
+     {
+       tym_libelle: "Image",
+     },
+     {
+       tym_libelle: "Vidéo",
+     },
+     {
+       tym_libelle: "Youtube",
+     },
+   ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
