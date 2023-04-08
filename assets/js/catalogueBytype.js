f286056cf66d22170b3178856187a591cd5fe2fd
pathymavuba <mavubapathy@gmail.com>
2023-04-08 20:20:07 +0100

    seeder/typeMedia:fix js of select on page bycategoorie and bytypecategorie

---
+++ assets/js/catalogueBytype.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var input_choix = document.getElementById("input");
+var choix = document.querySelector("#choix");
+var content = document.getElementById("content");
+var item_select = document.querySelectorAll(".item-choice");
+input_choix.addEventListener("click", function () {
+  content.classList.toggle("content-hide");
+  content.classList.add("content");
+});
+item_select.forEach(function (element) {
+  element.addEventListener("click", function () {
+    choix.textContent = element.textContent;
+  });
+});
