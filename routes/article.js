d5c209f4ac9f78900a0278f5cede13485663a97d
Trigo YODILA <trigoyodila1996@gmail.com>
2023-04-10 14:37:22 +0100

    feature/dynamisation-article : ajout des requêtes pour recupérer les datas

--- routes/article.js
+++ routes/article.js
@@ -1,9 +1,36 @@
 var express = require("express");
 var router = express.Router();
+const { Produit, Tarif } = require("../models");
 
-router.get("/:id", (req, res, next) => {
-  res.locals.titre = "article";
-  res.render("article/index");
+router.get("/:id", async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const article = await Produit.findOne({
+      where: {
+        pro_id: id,
+      },
+    });
+
+    const priceArticle = await Produit.findOne({
+      include: [
+        {
+          model: Tarif,
+          required: true,
+          attributes: ["tar_ttc"],
+        },
+      ],
+    });
+    // res.json({ priceArticle });
+    return res.render("article/index", {
+      article: article,
+      priceArticle: priceArticle,
+    });
+  } catch (error) {
+    res.status(500).render("article/index", {
+      error: true,
+      errorMsg: "une erreur est survenue ",
+    });
+  }
 });
 
 module.exports = router;
