642537b16f4c0a6aa75f4e719cb930a7bef819ab
seleshabani <seleshabani4@gmail.com>
2023-04-11 11:53:08 +0100

    feature/promotion : dynamisation de la page promotion

--- routes/promotion.js
+++ routes/promotion.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const {Apply, Promo, Produit ,Tarif, Media} = require('../models');
 
 router.get("/", async (req, res) => {
   res.locals.titre = "promotion";
-  res.render("promotion/index");
+  const ProduitEnPromos = await Apply.findAll({
+    limit:10,
+    include:[
+      {
+        model:Produit,
+        attributes:['pro_id','pro_libelle','pro_ref'],
+        include:[
+          { model: Media, attributes: ["med_id", "med_ressource"] },
+          { model: Tarif, attributes: ["tar_ttc"] },
+        ],
+      },
+      {
+        model:Promo,
+        attributes:['prm_code','prm_pourcent','prm_valeur'],
+        where:{
+          prm_actif:true
+        },
+      },
+    ],
+  });
+  res.render("promotion/index",{
+    produits:ProduitEnPromos,
+    nbrProduit:ProduitEnPromos.length
+  });
 });
 module.exports = router;
