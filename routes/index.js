fd7664b88e04afa80f1df4cb2b59fe03aa059be2
vict97 <myindavictoire@gmail.com>
2023-04-11 09:27:52 +0100

    feature/connexion:

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {Produit,Media,Tarif} = require('../models');
+const { Produit, Media, Tarif } = require("../models");
 
 /* GET home page. */
-router.get('/', async (req, res, next)=> {
-  res.locals.titre = "Accueil"
-  const produits = await Produit.findAll({limit:10,
-    order: [['pro_id', 'DESC'],],
-    include:[
-      {model:Media,attributes:['med_id','med_ressource']},
-      {model:Tarif,attributes:['tar_ht','tar_ttc']}
-    ]
+router.get("/", async (req, res, next) => {
+  res.locals.titre = "Accueil";
+  const produits = await Produit.findAll({
+    limit: 10,
+    order: [["pro_id", "DESC"]],
+    include: [
+      { model: Media, attributes: ["med_id", "med_ressource"] },
+      { model: Tarif, attributes: ["tar_ht", "tar_ttc"] },
+    ],
   });
-  res.render('default/index',{
-    produits:produits
+  res.render("default/index", {
+    produits: produits,
   });
 });
-
 
 module.exports = router;
