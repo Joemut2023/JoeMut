f2b7cca3ba191218f82de11ff0c36e411b640d1b
pathymavuba <mavubapathy@gmail.com>
2023-04-13 12:43:24 +0100

    review/13-04:add page 404

--- routes/error.js
+++ routes/error.js
@@ -6,5 +6,8 @@ router.get("/", async (req, res, next) => {
     res.locals.titre = "error";
     res.render("error/serverError");
 })
-
+router.get("/404",async function (re,res){
+    res.locals.titre = "error";
+    res.render("error/404")
+})
 module.exports = router;
