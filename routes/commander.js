88d398db4389984825bbc741aab337a005f9dc30
seleshabani <seleshabani4@gmail.com>
2023-04-13 13:17:07 +0100

    feature/commande : affichage dynamique de mode de livraison dans la page commande

--- routes/commander.js
+++ routes/commander.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {Adresse} = require('../models');
+const {Adresse,Frais_port} = require('../models');
 
 router.get("/", async (req, res) => {
   res.locals.titre = "commander";
@@ -10,6 +10,9 @@ router.get("/", async (req, res) => {
     return res.render('commander/index');
   }
   try {
+    const mode_livraisons = await Frais_port.findAll({
+      where : {frp_actif:true}
+    });
     if (userId) {
       const adresses = await Adresse.findAll({
         where: {
@@ -17,12 +20,16 @@ router.get("/", async (req, res) => {
         },
       });
       return res.render("commander/index",{
-        adresses
+        adresses,
+        mode_livraisons:mode_livraisons
       });
     }else{
-      return res.render('commander/index');
+      return res.render('commander/index',{
+        mode_livraisons:mode_livraisons
+      });
     }
   } catch (error) {
+    console.log(error);
     return res.render('commander/index');
   }
 });
