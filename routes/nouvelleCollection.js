86e77bcaf9d84c1e518d1aebf29ffb143e1f2b1d
pathymavuba <mavubapathy@gmail.com>
2023-04-13 17:45:35 +0100

    review/13-04:mise en place de la pagination sur la page nouvelle collection

--- routes/nouvelleCollection.js
+++ routes/nouvelleCollection.js
@@ -1,21 +1,48 @@
 const express = require("express");
 const router = express.Router();
 const { Produit, Media, Tarif } = require("../models");
+const { PAGINATION_LIMIT } = require("../helpers/utils_const");
+const check_paginate_value = require("../helpers/check_paginate_value");
 
 router.get("/", async (req, res) => {
-  res.locals.titre = "nouvelle collection";
-  const produits = await Produit.findAll({
-    limit: 8,
-    order: [["pro_id", "DESC"]],
-    include: [
-      { model: Media, attributes: ["med_id", "med_ressource"] },
-      { model: Tarif, attributes: ["tar_ht", "tar_ttc"] },
-    ],
-    where: { pro_new_collect: 1 },
-  });
-  res.render("nouvelleCollection/index", {
-    produits: produits,
-  });
+  let { page, start, end } = check_paginate_value(req);
+  try {
+    res.locals.titre = "nouvelle collection";
+    const nouveauProduits = await Produit.findAll({
+      offset: start,
+      limit: PAGINATION_LIMIT,
+      order: [["pro_id", "DESC"]],
+    });
+    const produits = await Produit.findAll({
+      // offset: start,
+      // limit: PAGINATION_LIMIT,
+      order: [["pro_id", "DESC"]],
+      include: [
+        { model: Media, attributes: ["med_id", "med_ressource"] },
+        { model: Tarif, attributes: ["tar_ht", "tar_ttc"] },
+      ],
+      where: { pro_new_collect: 1 },
+    });
+    let nbrPages = Math.ceil(produits.length / PAGINATION_LIMIT);
+    let next = start > 0 ? page + 1 : null;
+    let prev = end < 0 ? page - 1 : null;
+    res.render("nouvelleCollection/index", {
+      nouveauProduits: nouveauProduits,
+      produits: produits,
+      nbrPages: nbrPages,
+      pageActive: page,
+      start: start,
+      end: end,
+      prev: prev,
+      next: next,
+    });
+  } catch (error) {
+    res.status(500).render("error/serverError", {
+      error: true,
+      errorMsg: "Une erreur est survenue!",
+      detailError: error,
+    });
+  }
 });
 
 module.exports = router;
