7d57abb86a0f41a0e90458321787e49f1daaeb61
seleshabani <seleshabani4@gmail.com>
2023-04-13 13:01:10 +0100

    feature/commande : affichage des adresses dynamique dans la page commande

--- routes/commander.js
+++ routes/commander.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const {Adresse} = require('../models');
 
 router.get("/", async (req, res) => {
   res.locals.titre = "commander";
-  res.render("../views/commander");
+  let userId = req.session.userId;
+  let {newadress} = req.query;
+  if (newadress) {
+    return res.render('commander/index');
+  }
+  try {
+    if (userId) {
+      const adresses = await Adresse.findAll({
+        where: {
+          cli_id: req.session.userId,
+        },
+      });
+      return res.render("commander/index",{
+        adresses
+      });
+    }else{
+      return res.render('commander/index');
+    }
+  } catch (error) {
+    return res.render('commander/index');
+  }
 });
 module.exports = router;
