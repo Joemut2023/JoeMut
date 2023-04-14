58663eca0d0924b68f85cd5530c463bc7125d263
pathymavuba <mavubapathy@gmail.com>
2023-04-14 18:55:43 +0100

    feature/getPanier:create panier for user after inscription

--- routes/inscription.js
+++ routes/inscription.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const { M, MME } = require("../helpers/titre");
 const router = express.Router();
-const { Client } = require("../models");
+const { Client, Panier, Produit, Panier_detail } = require("../models");
+const  {Op } = require("sequelize")
 let bcrypt = require("bcryptjs");
+const { ACTIF } = require("../helpers/utils_const");
 router.get("/", (req, res, next) => {
   res.locals.titre = "inscription";
   res.render("inscription/index");
 });
 router.post("/", async (req, res, next) => {
-  var { tit_id, cli_nom, cli_prenom, cli_mail, cli_pwd } = req.body;
+  var { tit_id, cli_nom, cli_prenom, cli_mail, cli_pwd, panier_items } =
+    req.body;
   tit_id = tit_id === "M" ? M : MME;
   try {
     if (
@@ -17,7 +20,7 @@ router.post("/", async (req, res, next) => {
       cli_mail === "" ||
       cli_pwd === ""
     ) {
-      return res.render("inscription/index", {
+      return res.json({
         error: true,
         errorMsg: "Veillez remplir tout les champs",
       });
@@ -26,7 +29,7 @@ router.post("/", async (req, res, next) => {
       where: { cli_mail },
     });
     if (oldClient) {
-      return res.status(409).render("inscription/index", {
+      return res.status(409).json({
         error: true,
         errorMsg: "Un utilisateur existe déjà avec ce mail",
       });
@@ -39,13 +42,58 @@ router.post("/", async (req, res, next) => {
       cli_mail,
       cli_pwd: pwdhashed,
     });
+
+    //create for client panier
+    let oldPanier = await Panier.findOne({ where: { cli_id: client.cli_id } });
+    if (oldPanier) {
+      return res.status(409).json({ msg: "vous avez un panier" });
+    }
+    let panier = await Panier.create({
+      cli_id: client.cli_id,
+    });
+
+    // add in panier_detail
+    // panier_items.forEach(async (item) => {
+    //   let produit = await Produit.findByPk(item.pro_id, {
+    //     include: [
+    //       {
+    //         model: Tarif,
+    //         attributes: ["tar_ttc", "tar_id", "tar_ht"],
+    //         where: {
+    //           [Op.and]: [
+    //             {
+    //               pro_id: item.pro_id,
+    //             },
+    //             {
+    //               tar_statut: ACTIF,
+    //             },
+    //           ],
+    //         },
+    //       },
+    //     ],
+    //   });
+       
+    //   res.json({produit})
+    //   // insérer dans panier_details
+    //   // let panier_detail = await Panier_detail.create({
+    //   //   pro_id: produit.pro_id,
+    //   //   tar_id: produit.Tarifs[0].tar_id,
+    //   //   pan_id: panier.pan_id,
+    //   //   pad_qte: item.pad_qte,
+    //   //   pad_ht: produit.Tarifs[0].tar_ht,
+    //   // });
+    //   res.json({ panier_detail:panier_detail});
+    // });
+
+    req.session.panierId = panier.pan_id;
     req.session.userId = client.cli_id;
     res.locals.user = client;
     res.redirect(301, `/mon-compte`);
   } catch (error) {
-    res.render("inscription/index", {
+    res.status(500).render("error/serverError", {
       error: true,
       errorMsg: "Une erreur est survenue!",
+      detailError: error,
     });
   }
 });
