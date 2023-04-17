bbfe5e1ebd900cf47f388562fc48fa57fc2d77df
Trigo YODILA <trigoyodila1996@gmail.com>
2023-04-17 18:03:50 +0100

    feature/nouvelleCollection : modification des requetes produits et nouveau produits dans la route nouvelle collection et filtrage dynamique

--- routes/nouvelleCollection.js
+++ routes/nouvelleCollection.js
@@ -6,24 +6,46 @@ const check_paginate_value = require("../helpers/check_paginate_value");
 
 router.get("/", async (req, res) => {
   let { page, start, end } = check_paginate_value(req);
+  let orderby = req.query.orderby;
+  var orderCondition;
+  let choix;
+  if (orderby === "AàZ") {
+    orderCondition = [["pro_libelle", "ASC"]];
+    choix = "Nom, A à Z";
+  } else if (orderby === "ZàA") {
+    orderCondition = [["pro_libelle", "DESC"]];
+    choix = "Nom, Z à A";
+  } else if (orderby === "PC") {
+    orderCondition = [[Tarif, "tar_ttc", "ASC"]];
+    choix = "Prix, croissant";
+  } else if (orderby === "PD") {
+    orderCondition = [[Tarif, "tar_ttc", "DESC"]];
+    choix = "Prix, décroissant";
+  } else {
+    orderCondition = [["pro_libelle", "DESC"]];
+    choix = "Choisir";
+  }
   try {
     res.locals.titre = "nouvelle collection";
     const nouveauProduits = await Produit.findAll({
-      offset: start,
-      limit: PAGINATION_LIMIT,
-      order: [["pro_id", "DESC"]],
-    });
-    const produits = await Produit.findAll({
       // offset: start,
       // limit: PAGINATION_LIMIT,
-      order: [["pro_id", "DESC"]],
+      // order: [["pro_id", "DESC"]],
+      where: { pro_new_collect: 1 },
+    });
+    const produits = await Produit.findAll({
+      offset: start,
+      limit: PAGINATION_LIMIT,
+      // order: [["pro_id", "DESC"]],
+      order:orderCondition,
       include: [
         { model: Media, attributes: ["med_id", "med_ressource"] },
         { model: Tarif, attributes: ["tar_ht", "tar_ttc"] },
       ],
       where: { pro_new_collect: 1 },
+      
     });
-    let nbrPages = Math.ceil(produits.length / PAGINATION_LIMIT);
+    let nbrPages = Math.ceil(nouveauProduits.length / PAGINATION_LIMIT);
     let next = start > 0 ? page + 1 : null;
     let prev = end < 0 ? page - 1 : null;
     res.render("nouvelleCollection/index", {
@@ -35,6 +57,8 @@ router.get("/", async (req, res) => {
       end: end,
       prev: prev,
       next: next,
+      choix: choix,
+      orderby: orderby,
     });
   } catch (error) {
     res.status(500).render("error/serverError", {
