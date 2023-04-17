dece386fb57ed259a5680679dd4fa6e4b49e5c7b
vict97 <myindavictoire@gmail.com>
2023-04-17 12:24:57 +0100

    panienDetail: add post router panierDetail

--- routes/panierDetail.js
+++ routes/panierDetail.js
@@ -1,15 +1,25 @@
 var express = require("express");
 var router = express.Router();
-const { Op } = require("sequelize");
+const { Op, and } = require("sequelize");
 const { Panier_detail, Apply, Tarif, Promo, Quantite } = require('../models');
 
 
-router.get("/", async (req, res, next) => {
-    const { pro_id, pad_qte } = req.body
+router.post("/", async (req, res, next) => {
+    const { pro_id, pad_qte } = req.body;
+    const pan_id = req.session.panierId;
     try {
-        const oldPanierDetail = await Panier_detail.findOne({ where: { pro_id } });
+        const oldPanierDetail = await Panier_detail.findOne({
+            where:
+            {
+                [Op.and]: [
+                    { pro_id },
+                    { pan_id }
+                ]
+            }
+        });
+
         const quantite = await Quantite.findOne({ where: { pro_id } });
-        if (!oldPanierDetail) {
+        if (oldPanierDetail == null) {
             const promo = await Apply.findOne({
                 where: { pro_id },
                 include: [{
@@ -17,42 +27,49 @@ router.get("/", async (req, res, next) => {
                     attributes: ["prm_pourcent"],
                     where: { prm_actif: true }
                 }]
-            })
+            });
             let prm_id = promo ? promo.prm_id : null;
             let pad_remise = promo ? promo.Promo.prm_pourcent : null;
             const tarif = await Tarif.findOne({ where: { pro_id } });
             let tar_id = tarif.tar_id;
             let pad_ht = tarif.tar_ht;
             let pad_ttc = tarif.tar_ttc;
-            console.log(tarif, "tarif");
-
-
-            // if (quantite) {
-            //     let quantiteDispo = quantite.qua_nbre;
-            //     if (newQuantite > quantiteDispo) {
-            //         return res.status(409).send(`La quantité disponible n'est que de ${quantiteDispo} articles`)
-            //     }
-            // }
             const panierDetail = await Panier_detail.create({
-                pro_id, tar_id, prm_id, pan_id: req.session.panierId, pad_qte, pad_ht, pad_ttc, pad_remise
-            })
-            return res.status(201).json(panierDetail)
+                pro_id, tar_id, prm_id, pan_id, pad_qte, pad_ht, pad_ttc, pad_remise
+            });
+            return res.status(201).json({ panierDetail });
         }
-        const pad_id = oldPanierDetail.pad_id
-        const newQuantite = oldPanierDetail.pad_qte + pad_qte
-        // if (quantite) {
-        //     let quantiteDispo = quantite.qua_nbre;
-        //     if (newQuantite > quantiteDispo) {
-        //         return res.status(409).send(`La quantité disponible n'est que de ${quantiteDispo} articles`)
-        //     }
-        // }
+
+        const newQuantite = oldPanierDetail.pad_qte + pad_qte;
+
+
+        if (quantite !== null) {
+            let quantiteDispo = quantite.qua_nbre;
+            if (newQuantite > quantiteDispo) {
+                return res.status(409).send(`La quantité disponible n'est que de ${quantiteDispo} articles`)
+            }
+        }
+
         const panierDetail = await Panier_detail.update({
             pad_qte: newQuantite
-        }, { where: { pad_id } })
-        const newPanierDetail = await Panier_detail.findOne({ where: { pro_id } })
+        }, { where: { pad_id: oldPanierDetail.pad_id } })
 
-        return res.status(200).json({ panierDetail, newPanierDetail })
+        if (panierDetail[0] == 1) {
+            const newPanierDetail = await Panier_detail.findOne({
+                where:
+                {
+                    [Op.and]: [
+                        { pro_id },
+                        { pan_id }
+                    ]
+                }
+            });
+            return res.status(200).json({ newPanierDetail: newPanierDetail });
+        }
+
+        return res.status(404).send("aucun produit ajouté");
     } catch (error) {
+
         res.status(500).json({ error: error })
     }
 })
@@ -68,6 +85,9 @@ router.get("/", async (req, res, next) => {
 //         res.status(500).json({ error: error })
 //     }
 // })
+
+
+
 
 module.exports = router
 
