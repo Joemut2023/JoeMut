e6a1b39a1f15b7df4999b7edb9580fad7403e045
Romain kabasi <kabasiromain@gmail.com>
2023-04-18 16:03:20 +0100

    develop/romain : fix bug

--- routes/connexion.js
+++ routes/connexion.js
@@ -26,7 +26,7 @@ router.post("/", async (req, res) => {
     if (!valid) {
       return res.json('mot de passe incorrect');
     }
-    const panier = await Panier.findOne({
+    var panier = await Panier.findOne({
       where: { cli_id: client.cli_id },
       include: [{ model: Commande, required: false,where:{com_id:null} }],
      // where: { '$Commande.com_id$': null }
