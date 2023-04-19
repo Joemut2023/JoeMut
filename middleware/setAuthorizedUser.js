42b865b8024c34b3db8a28c2140b0a31ebafe42d
seleshabani <seleshabani4@gmail.com>
2023-04-19 13:08:49 +0100

    feature/commande : recuperation du user dans res.locals dans toute l'application, design page commande

--- middleware/setAuthorizedUser.js
+++ middleware/setAuthorizedUser.js
@@ -1,7 +1,10 @@
+const {Client} = require('../models');
+
 const setAuthorizedUser = async (req,res,next)=>{
     const UsersSession = req.session.userId;
     if (UsersSession) {
-        res.locals.user = UsersSession;
+        let client = await Client.findByPk(UsersSession);
+        res.locals.user = client;
     }
     next();
 }
