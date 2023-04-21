6b878db8ca433387f8aab99a26eb9f0aa45176a2
pathymavuba <mavubapathy@gmail.com>
2023-04-21 17:09:28 +0100

    dynamisation du pdf

--- routes/devis.js
+++ routes/devis.js
@@ -7,3 +7,22 @@ router.get("/", async (req, res, next) => {
 })
 
 module.exports = router;
+
+
+// var express = require("express");
+// var router = express.Router();
+// const ejs = require("ejs");
+// const fs = require("fs");
+// const path = require("path");
+
+// router.get("/", async (req, res, next) => {
+//   console.log(req.session.commandeId);
+//   let ejsdevis = fs.readFileSync(
+//     path.join(__dirname, "../mailTemplate/devis.ejs"),
+//     "utf8"
+//   );
+//   ejs.render(ejsdevis);
+//   //res.render("devis/index");
+// });
+
+// module.exports = router;
