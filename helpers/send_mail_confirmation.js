df8debbfd921184ec7adc61815636b085a5f0186
seleshabani <seleshabani4@gmail.com>
2023-04-21 16:57:22 +0100

    feature/pdf : generation du pdf avec puppeteer

--- helpers/send_mail_confirmation.js
+++ helpers/send_mail_confirmation.js
@@ -2,6 +2,8 @@ var nodemailer = require("nodemailer");
 const fs = require("fs");
 const path = require("path");
 const ejs = require("ejs");
+const pdf = require('pdfkit');
+const puppeteer = require('puppeteer');
 
 module.exports = async (
   res,
@@ -24,7 +26,6 @@ module.exports = async (
     path.join(__dirname, "../mailTemplate/devis.ejs"),
     "utf8"
   );
-
   let html = ejs.render(ejsFile, {
     commande: commande,
     adresseLiv: adresseLiv,
@@ -36,8 +37,7 @@ module.exports = async (
     taxe: taxe,
     totalTTC:totalTTC,
   });
-
-  let pdf = ejs.render(ejsdevis, {
+  let pdfview =  ejs.renderFile(path.join(__dirname, "../mailTemplate/devis.ejs"), {
     commande: commande,
     adresseLiv: adresseLiv,
     adresseFac: adresseFac,
@@ -48,7 +48,11 @@ module.exports = async (
     taxe,
     totalTTC,
   });
-
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`${ process.env.APP_URL}devis`);
+  await page.pdf({path:path.join(__dirname, "../public/pdf/devis/"+commande.com_id+".pdf")});
+  await page.close();
   try {
     const transporter = nodemailer.createTransport({
       service: "gmail",
