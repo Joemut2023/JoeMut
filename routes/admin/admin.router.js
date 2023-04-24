369004a62a4c04f71116a821fd9a22e294826a5b
seleshabani <seleshabani4@gmail.com>
2023-04-24 16:12:56 +0100

    feature/admin-layout : layout for admin

--- routes/admin/admin.router.js
+++ routes/admin/admin.router.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const loginRouter = require('./admin.connexion');
 const devisRouter = require('./admin.devis');
 const produitsRouter = require('./admin.produits');
+const dashboardRouter = require('./admin.dashboard');
 const adminIsAuth = require("../../middleware/admin/admin.auth");
 const adminLayout = require("../../middleware/admin/admin.layout");
 
 router.use('/login',loginRouter);
 router.use('/devis',[adminLayout],devisRouter);
 router.use('/produits',[adminLayout],produitsRouter);
+router.use('/dashboard',[adminLayout],dashboardRouter);
 
 module.exports = router;
