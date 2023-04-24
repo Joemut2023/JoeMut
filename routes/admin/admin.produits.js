81ad646aa94fb72162aecf5069f84bc6d8edc863
Romain kabasi <kabasiromain@gmail.com>
2023-04-24 16:54:06 +0100

    feature/admin-produits : create table

--- routes/admin/admin.produits.js
+++ routes/admin/admin.produits.js
@@ -1,8 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { Produit, Quantite, Tarif, Media, Categorie } = require("../../models");
+const { PAGINATION_LIMIT } = require("../../helpers/utils_const");
 
-router.get('/',(req,res)=>{
-    res.render('produits/index');
+router.get("/", async (req, res) => {
+  try {
+    const produits = await Produit.findAll({
+      limit: 5,
+      include: [
+        { model: Quantite, attributes: ["qua_nbre"] },
+        { model: Tarif, attributes: ["tar_ht", "tar_ttc"] },
+        { model: Media, attributes: ["med_ressource"] },
+        { model: Categorie, attributes: ["cat_libelle"] },
+      ],
+    });
+
+    res.render("produits/index", {
+      produits: produits,
+    });
+    // return res.status(200).send(produits);
+  } catch (error) {
+    res.status(500).render("article/index", {
+      error: true,
+      errorMsg: "une erreur est survenue ",
+    });
+  }
 });
 
 module.exports = router;
