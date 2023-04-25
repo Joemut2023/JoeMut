9fac8258f31857d6e37df8d382b8b751867503b2
Romain kabasi <kabasiromain@gmail.com>
2023-04-25 13:11:28 +0100

    develop/romain : fix quantité

--- routes/admin/admin.produits.js
+++ routes/admin/admin.produits.js
@@ -4,21 +4,42 @@ const { Produit, Quantite, Tarif, Media, Categorie } = require("../../models");
 const { PAGINATION_LIMIT } = require("../../helpers/utils_const");
 
 router.get("/", async (req, res) => {
+  let quantiteOfEachProduct = [];
   try {
     const produits = await Produit.findAll({
-      limit: 5,
+      limit: 20,
       include: [
-        { model: Quantite, attributes: ["qua_nbre",] },
+        { model: Quantite, attributes: ["qua_nbre"] },
         { model: Tarif, attributes: ["tar_ht", "tar_ttc"] },
         { model: Media, attributes: ["med_ressource"] },
         { model: Categorie, attributes: ["cat_libelle"] },
       ],
     });
+    const allProduits = await Produit.findAll({
+      include: [
+        { model: Quantite, attributes: ["qua_nbre"] },
+        { model: Tarif, attributes: ["tar_ht", "tar_ttc"] },
+        { model: Media, attributes: ["med_ressource"] },
+        { model: Categorie, attributes: ["cat_libelle"] },
+      ],
+    });
+    for (let index = 0; index < allProduits.length; index++) {
+      const quantiteInitial = await Quantite.sum("qua_nbre", {
+        where: {
+          pro_id: allProduits[index].pro_id,
+        },
+      });
+      quantiteOfEachProduct.push({
+        id: allProduits[index].pro_id,
+        qty: quantiteInitial,
+      });
+    }
 
     res.render("produits/index", {
       produits: produits,
+      quantiteOfEachProduct: quantiteOfEachProduct,
     });
-    //  return res.status(200).send(produits);
+    // return res.status(200).json({ produits, quantiteOfEachProduct });
   } catch (error) {
     res.status(500).render("article/index", {
       error: true,
