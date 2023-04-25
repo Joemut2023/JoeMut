209a5ba7a49911f92545ce91e93a388712937f6b
Romain kabasi <kabasiromain@gmail.com>
2023-04-25 09:23:35 +0100

    feature/admin-produits : get all products

--- routes/admin/admin.produits.js
+++ routes/admin/admin.produits.js
@@ -8,17 +8,17 @@ router.get("/", async (req, res) => {
     const produits = await Produit.findAll({
       limit: 5,
       include: [
-        { model: Quantite, attributes: ["qua_nbre"] },
+        { model: Quantite, attributes: ["qua_nbre",] },
         { model: Tarif, attributes: ["tar_ht", "tar_ttc"] },
         { model: Media, attributes: ["med_ressource"] },
         { model: Categorie, attributes: ["cat_libelle"] },
       ],
     });
 
-    res.render("produits/index", {
-      produits: produits,
-    });
-    // return res.status(200).send(produits);
+    // res.render("produits/index", {
+    //   produits: produits,
+    // });
+     return res.status(200).send(produits);
   } catch (error) {
     res.status(500).render("article/index", {
       error: true,
