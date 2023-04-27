f2a559b06dc0ec5434e3dbc5ddcf577544a559da
Trigo YODILA <trigoyodila1996@gmail.com>
2023-04-27 17:55:56 +0100

    feature/ajoutProduit : ajout tabs et choix dynamique

---
+++ assets/js/admin/ajoutProduit.js
@@ -0,0 +1,33 @@
+const button1 = document.querySelector(".btn-tab-1");
+const button2 = document.querySelector(".btn-tab-2");
+const button3 = document.querySelector(".btn-tab-3");
+const button4 = document.querySelector(".btn-tab-4");
+
+button1.addEventListener("click", function () {
+  button1.classList.add("clicked");
+  button2.classList.remove("clicked");
+  button3.classList.remove("clicked");
+  button4.classList.remove("clicked");
+});
+
+button2.addEventListener("click", function(){
+    button1.classList.remove("clicked")
+    button2.classList.add("clicked")
+    button3.classList.remove("clicked")
+    button4.classList.remove("clicked")
+})
+
+button3.addEventListener("click", function () {
+  button1.classList.remove("clicked");
+  button3.classList.add("clicked");
+  button2.classList.remove("clicked");
+  button4.classList.remove("clicked");
+});
+
+button4.addEventListener("click", function () {
+  button1.classList.remove("clicked");
+  button4.classList.add("clicked");
+  button2.classList.remove("clicked");
+  button3.classList.remove("clicked");
+});
+
---
+++ public/javascripts/admin/ajoutProduit.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var button1 = document.querySelector(".btn-tab-1");
+var button2 = document.querySelector(".btn-tab-2");
+var button3 = document.querySelector(".btn-tab-3");
+var button4 = document.querySelector(".btn-tab-4");
+button1.addEventListener("click", function () {
+  button1.classList.add("clicked");
+  button2.classList.remove("clicked");
+  button3.classList.remove("clicked");
+  button4.classList.remove("clicked");
+});
+button2.addEventListener("click", function () {
+  button1.classList.remove("clicked");
+  button2.classList.add("clicked");
+  button3.classList.remove("clicked");
+  button4.classList.remove("clicked");
+});
+button3.addEventListener("click", function () {
+  button1.classList.remove("clicked");
+  button3.classList.add("clicked");
+  button2.classList.remove("clicked");
+  button4.classList.remove("clicked");
+});
+button4.addEventListener("click", function () {
+  button1.classList.remove("clicked");
+  button4.classList.add("clicked");
+  button2.classList.remove("clicked");
+  button3.classList.remove("clicked");
+});
