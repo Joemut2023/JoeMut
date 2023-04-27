1bdb2d27259653576857aaf92f4eb2568d4c4aff
Romain kabasi <kabasiromain@gmail.com>
2023-04-27 14:25:50 +0100

    resolve conflict in admin.router

--- routes/admin/admin.router.js
+++ routes/admin/admin.router.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const loginRouter = require('./admin.connexion');
-const devisRouter = require('./admin.devis');
-const produitsRouter = require('./admin.produits');
-const dashboardRouter = require('./admin.dashboard');
-const panierRouter = require('./admin.panier');
+const adminIsAuth = require("../../middleware/admin/admin.auth");
+const adminLayout = require("../../middleware/admin/admin.layout");
+const loginRouter = require("./admin.connexion");
+const devisRouter = require("./admin.devis");
+const produitsRouter = require("./admin.produits");
+const dashboardRouter = require("./admin.dashboard");
+const panierRouter = require("./admin.panier");
 const taillesRouter = require("./admin.taille");
 
-
-router.use('/login',loginRouter);
-router.use('/devis',[adminLayout],devisRouter);
-router.use('/produits',[adminLayout],produitsRouter);
-router.use('/dashboard',[adminLayout],dashboardRouter);
-router.use('/paniers',[adminLayout],panierRouter);
-router.use("/tailles/",[adminLayout], taillesRouter);
+router.use("/login", loginRouter);
+router.use("/devis", [adminLayout], devisRouter);
+router.use("/produits", [adminLayout], produitsRouter);
+router.use("/dashboard", [adminLayout], dashboardRouter);
+router.use("/paniers", [adminLayout], panierRouter);
+router.use("/tailles/", [adminLayout], taillesRouter);
 
 module.exports = router;
