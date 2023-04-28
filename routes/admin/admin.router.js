4b523798801d974c565a0ac63188c8e0b3662fb7
vict97 <myindavictoire@gmail.com>
2023-04-28 18:39:17 +0100

    develop:fix conflit css

--- routes/admin/admin.router.js
+++ routes/admin/admin.router.js
@@ -4,12 +4,8 @@ const loginRouter = require("./admin.connexion");
 const devisRouter = require("./admin.devis");
 const produitsRouter = require("./admin.produits");
 const categoriesRouter = require("./admin.categories");
-<<<<<<< HEAD
 const fraisPortRouter = require("./admin.fraisport")
 const dashboardRouter = require('./admin.dashboard');
-=======
-const dashboardRouter = require("./admin.dashboard");
->>>>>>> 6a3817ab81103e263ff84709a8bfa2aa6adaa6c4
 const adminIsAuth = require("../../middleware/admin/admin.auth");
 const adminLayout = require("../../middleware/admin/admin.layout");
 const panierRouter = require("./admin.panier");
@@ -24,17 +20,10 @@ router.use("/dashboard", [adminLayout], dashboardRouter);
 router.use("/paniers", [adminLayout], panierRouter);
 router.use("/categories", [adminLayout], categoriesRouter);
 router.use("/dashboard", [adminLayout], dashboardRouter);
-<<<<<<< HEAD
 router.use("/tailles/", [adminLayout], taillesRouter);
-router.use("/couleurs/", [adminLayout], CouleurRouter);
+router.use("/couleurs/", [adminLayout], couleurRouter);
 router.use('/type-categories', [adminLayout], categoriesRouter);
 router.use('/frais-port', [adminLayout], fraisPortRouter)
-
-=======
-router.use("/tailles", [adminLayout], taillesRouter);
-router.use("/couleurs", [adminLayout], couleurRouter);
-router.use("/type-categories", [adminLayout], categoriesRouter);
 router.use("/factures", [adminLayout], factureRouter);
->>>>>>> 6a3817ab81103e263ff84709a8bfa2aa6adaa6c4
 
 module.exports = router;
