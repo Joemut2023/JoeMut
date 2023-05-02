0b6cda8db7fdfb8cb3a7eabd59dd9f78ba8240f7
Romain kabasi <kabasiromain@gmail.com>
2023-05-02 16:23:01 +0100

    feature/admin-client : add pagination

--- routes/admin/admin.client.js
+++ routes/admin/admin.client.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const { Client, Titre } = require("../../models");
+const { PAGINATION_LIMIT_ADMIN } = require("../../helpers/utils_const");
+const check_admin_paginate_value = require("../../helpers/check_admin_paginate_value");
 router.get("/", async (req, res) => {
+  let { page, start, end } = check_admin_paginate_value(req);
   try {
     const clients = await Client.findAll({
+      offset: start,
+      limit: PAGINATION_LIMIT_ADMIN,
       include: [{ model: Titre, attributes: ["tit_libelle"] }],
     });
-    res.render("client/index", { clients });
+    const AllClients = await Client.findAll();
+    let nbrPages = Math.ceil(AllClients.length / PAGINATION_LIMIT_ADMIN);
+    res.render("client/index", {
+      clients,
+      nbrPages,
+      pageActive: page,
+      start,
+      end,
+      produitsNbr: AllClients.length,
+    });
   } catch (error) {
     res.status(500).render("factures/index", {
       error: true,
