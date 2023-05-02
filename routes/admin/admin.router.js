0c0283aab7b60d5dee70bf6ab631a972909b7934
Romain kabasi <kabasiromain@gmail.com>
2023-05-02 14:15:41 +0100

    feature/admin-adresse : add view adresse

--- routes/admin/admin.router.js
+++ routes/admin/admin.router.js
@@ -4,14 +4,15 @@ const loginRouter = require("./admin.connexion");
 const devisRouter = require("./admin.devis");
 const produitsRouter = require("./admin.produits");
 const categoriesRouter = require("./admin.categories");
-const fraisPortRouter = require("./admin.fraisport")
-const dashboardRouter = require('./admin.dashboard');
+const fraisPortRouter = require("./admin.fraisport");
+const dashboardRouter = require("./admin.dashboard");
 const adminIsAuth = require("../../middleware/admin/admin.auth");
 const adminLayout = require("../../middleware/admin/admin.layout");
 const panierRouter = require("./admin.panier");
 const taillesRouter = require("./admin.taille");
 const couleurRouter = require("./admin.couleurs");
 const factureRouter = require("./admin.facture");
+const adresseRouter = require("./admin.adresse");
 
 router.use("/login", loginRouter);
 router.use("/devis", [adminLayout], devisRouter);
@@ -22,8 +23,9 @@ router.use("/categories", [adminLayout], categoriesRouter);
 router.use("/dashboard", [adminLayout], dashboardRouter);
 router.use("/tailles/", [adminLayout], taillesRouter);
 router.use("/couleurs/", [adminLayout], couleurRouter);
-router.use('/type-categories', [adminLayout], categoriesRouter);
-router.use('/frais-port', [adminLayout], fraisPortRouter)
+router.use("/type-categories", [adminLayout], categoriesRouter);
+router.use("/frais-port", [adminLayout], fraisPortRouter);
 router.use("/factures", [adminLayout], factureRouter);
+router.use("/adresse", [adminLayout], adresseRouter);
 
 module.exports = router;
