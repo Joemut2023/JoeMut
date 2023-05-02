ad7aa4cbe85884d19a7dd6d84cb8b3a62c592f07
seleshabani <seleshabani4@gmail.com>
2023-05-02 17:26:42 +0100

    feature/commande-view : editeur wysiwg

---
+++ public/javascripts/admin/devis.view.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var simplemde = new SimpleMDE({
+  element: document.querySelector("#devis-note-textarea"),
+  forceSync: true,
+  spellChecker: false,
+  initialValue: "Tapez ici..."
+});
