feb5b63d9bcd5ec8d041ed940951778470af0f19
pathymavuba <mavubapathy@gmail.com>
2023-05-03 10:44:44 +0100

    migration/transporteur:create table transporteur

---
+++ models/transporteur.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Transporteur extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Transporteur.init(
+    {
+      trs_id: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        autoIncrement:true
+      },
+      trs_libelle: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Transporteur",
+      timestamps:false
+    }
+  );
+  return Transporteur;
+};
