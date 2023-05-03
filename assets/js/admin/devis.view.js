647da37a12fe1d7c3c4038670a7726cbe7df128e
seleshabani <seleshabani4@gmail.com>
2023-05-03 15:15:12 +0100

    feature/commande : dynamisation liste des panier details

--- assets/js/admin/devis.view.js
+++ assets/js/admin/devis.view.js
@@ -2,6 +2,8 @@ const btnDocFormNote = document.querySelector('.btn-form-doc-note');
 const btnDocFormPaiement = document.querySelector('.btn-form-doc-paiement');
 const trFormNote = document.querySelector('.tr-form-note');
 const trFormPaiement = document.querySelector('.tr-form-paiement');
+const btnEditProds = document.querySelectorAll('.btn-edit-prod');
+
 (()=>{
     var simplemde = new SimpleMDE({
         element: document.querySelector("#devis-note-textarea"),
@@ -16,4 +18,13 @@ btnDocFormNote.addEventListener('click',(e)=>{
 btnDocFormPaiement.addEventListener('click',(e)=>{
     trFormPaiement.style.display === ''?trFormPaiement.style.display='table-row':trFormPaiement.style.display='';
 })
+btnEditProds.forEach(btnEditProd=>{
+    btnEditProd.addEventListener('click',(e)=>{
+        let padId = e.target.dataset.pad;
+        const trFormEditPro = document.querySelector(`.tr-form-edit-prod-${padId}`);
+        console.log(`.tr-form-edit-prod-${padId}`,padId);
+        trFormEditPro.style.display === ''?trFormEditPro.style.display='table-row':trFormEditPro.style.display='';
+    })
+},true)
+
 
