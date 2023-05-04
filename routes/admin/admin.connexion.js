f881cc60185a15175eed22d9ba98febc9a3f3fc3
seleshabani <seleshabani4@gmail.com>
2023-05-04 12:05:53 +0100

    feature/user-login : connexion par la base des données

--- routes/admin/admin.connexion.js
+++ routes/admin/admin.connexion.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const ejs = require("ejs");
 const path = require("path");
 const { EMAIL, PASSWORD } = require('../../helpers/infoUser');
+const {User} = require('../../models');
+let bcrypt = require("bcryptjs");
 //app.set("layout", path.join(__dirname, "views/layout/layout"));
 //app.set("views", path.join(__dirname, "views/admin"));
 router.get('/',async (req,res)=>{
@@ -14,37 +16,60 @@ router.post('/',async function(req,res){
        path.join(__dirname, "../../views/admin/login/index.ejs")
      );
     const {usr_email,usr_pwd} = req.body
+
     try {
+        
         if(usr_email === "" || usr_pwd === "") {
            let result = await ejs.renderFile(path.join(__dirname, "../../views/admin/login/index.ejs"), {
                errorMssg: "remplissez tous les champs",
              }); 
             return res.send(result)
         }
-       if(usr_email !== EMAIL) {
-         let result = await ejs.renderFile(
-           path.join(__dirname, "../../views/admin/login/index.ejs"),
-           {
-             errorMssg: "mail ou mot de passe incorrect",
-           }
-         );
-         return res.send(result);
-       }
-           if (usr_pwd !== PASSWORD) {
-             let result = await ejs.renderFile(
-               path.join(__dirname, "../../views/admin/login/index.ejs"),
-               {
-                 errorMssg: "mail ou mot de passe incorrect",
-               }
-             );
-             return res.send(result);
-           }
+
+        let user = await User.findOne({
+          where: { usr_mail: usr_email },
+        })
+
+        if(!user){
+          let result = await ejs.renderFile(
+            path.join(__dirname, "../../views/admin/login/index.ejs"),
+            {
+              errorMssg: "mail ou mot de passe incorrect",
+            }
+          );
+          return res.send(result);
+        }
+
+        const valid = await bcrypt.compare(usr_pwd, user.usr_pwd);
+
+        if (!valid) {
+          let result = await ejs.renderFile(
+            path.join(__dirname, "../../views/admin/login/index.ejs"),
+            {
+              errorMssg: "mail ou mot de passe incorrect",
+            }
+          );
+          return res.send(result);
+        }
            //
-          res.redirect("/admin/devis");
+        req.session.adminId = user.usr_id;
+        res.redirect("/admin/devis");
     
     } catch (error) {
-        
+        console.log(error);
+        let result = await ejs.renderFile(
+            path.join(__dirname, "../../views/admin/login/index.ejs"),
+            {
+              errorMssg: "Erreur interne",
+            }
+        );
+        return res.send(result);
     }
+})
+router.get('/deconnexion',async (req,res)=>{
+  req.session.destroy();
+  //delete res.locals.user;
+  res.redirect("/admin/login");
 })
 
 module.exports = router;
