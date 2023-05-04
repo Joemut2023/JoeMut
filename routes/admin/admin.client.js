83c7feba994f6b2aa50affade0d823a977e86286
Romain kabasi <kabasiromain@gmail.com>
2023-05-04 18:52:34 +0100

    feature/admin-client : updatclient

--- routes/admin/admin.client.js
+++ routes/admin/admin.client.js
@@ -29,11 +29,11 @@ router.get("/", async (req, res) => {
     });
   }
 });
-router.delete("/", async (req, res) => {
-  console.log(req.params, "params");
-  console.log(req.query, "query");
+router.post("/delete", async (req, res) => {
+  const cli_id = req.body.cli_id;
   try {
-    // const client = Client.destroy({ where: { cli_id } });
+    await Client.destroy({ where: { cli_id } });
+    res.redirect("/admin/clients");
   } catch (error) {
     res.status(500).render("client/index", {
       error: true,
@@ -45,11 +45,12 @@ router.get("/add", async (req, res) => {
   try {
     const titres = await Titre.findAll();
     res.render("client/addClient", { titres });
-  } catch (error) {}
-  res.status(500).render("client/index", {
-    error: true,
-    errorMsg: "une erreur est survenue ",
-  });
+  } catch (error) {
+    res.status(500).render("client/index", {
+      error: true,
+      errorMsg: "une erreur est survenue ",
+    });
+  }
 });
 router.post("/add", async (req, res) => {
   const { cli_prenom, cli_nom, cli_pwd, cli_mail, cli_activation, tit_id } =
@@ -78,6 +79,64 @@ router.post("/add", async (req, res) => {
     }
   } catch (error) {
     res.status(500).render("client/addClient", {
+      error: true,
+      errorMsg: "une erreur est survenue ",
+    });
+  }
+});
+
+router.get("/update", async (req, res) => {
+  const cli_id = req.query.cli_id;
+  try {
+    const titres = await Titre.findAll();
+    const clients = await Client.findOne({ where: { cli_id } });
+    res.render("client/updateClient", { titres, clients });
+  } catch (error) {
+    res.status(500).render("client/updateClient", {
+      error: true,
+      errorMsg: "une erreur est survenue ",
+    });
+  }
+});
+router.post("/update", async (req, res) => {
+  const {
+    cli_prenom,
+    cli_nom,
+    cli_num,
+    cli_pwd,
+    cli_mail,
+    cli_activation,
+    tit_id,
+    cli_newsletter,
+    cli_partenaire,
+    cli_id,
+  } = req.body;
+  console.log(req.body);
+  try {
+    const titres = await Titre.findAll();
+    const update = await Client.update(
+      {
+        cli_prenom,
+        cli_nom,
+        cli_num,
+        cli_pwd,
+        cli_mail,
+        cli_activation,
+        tit_id,
+        cli_newsletter,
+        cli_partenaire,
+      },
+      { where: { cli_id } }
+    );
+    const clients = await Client.findOne({ where: { cli_id } });
+    if (update[0] == 1) {
+      const succesMsg = "les informations ont été mises à jour avec succès";
+      return res.render("client/updateClient", { titres, clients, succesMsg });
+    }
+    const nothingMsg = "aucune nouvelle information trouvée ";
+    res.render("client/updateClient", { titres, clients,nothingMsg });
+  } catch (error) {
+    res.status(500).render("client/updateClient", {
       error: true,
       errorMsg: "une erreur est survenue ",
     });
