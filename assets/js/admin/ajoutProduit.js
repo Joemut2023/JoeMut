53e51518eaf942bb51d52eb8b3dccf607c685097
Trigo YODILA <trigoyodila1996@gmail.com>
2023-05-04 18:09:56 +0100

    refactor/ajoutProduit : suppression des sections stock, preferences disponibilité sur l'onglet quantité

--- assets/js/admin/ajoutProduit.js
+++ assets/js/admin/ajoutProduit.js
@@ -39,54 +39,7 @@ button4.addEventListener("click", function () {
   button3.classList.remove("clicked");
 });
 
-function appendLine() {
-  const line = document.createElement("div");
-  line.classList.add("row");
-  line.classList.add("line");
 
-  line.innerHTML = `
-  <div class="col-md-4">
-                    <select
-                      class="form-select"
-                      aria-label="Default select example"
-                    >
-                      <option selected>Choisissez une caracteristique</option>
-                      <option value="1">One</option>
-                      <option value="2">Two</option>
-                      <option value="3">Three</option>
-                    </select>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group has-feedback has-feedback-left">
-                      <input
-                        type="number"
-                        class="form-control"
-                        placeholder="Choisissez une valeur"
-                      />
-                    </div>
-                  </div>
-                  <div class="col-md-4 person">
-                    <div class="form-group">
-                      <input type="text" class="form-control" />
-                      <span class="delete delete-add col-md-1">
-                        <i class="fa-solid fa-trash"></i>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-  `;
-  lines.appendChild(line);
-}
-
-btn_add.addEventListener("click", function () {
-  appendLine();
-});
-
-// btns_delete.forEach((element) => {
-//   element.addEventListener("click", function () {
-//     console.log("BONJOUR ");
-//   });
-// });
 
 // traitement image
 const inputDiv = document.querySelector(".images");
