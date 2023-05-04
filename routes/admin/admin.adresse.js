69aef8a347f54da09a5eabbbab54fc45559f25db
Trigo YODILA <trigoyodila1996@gmail.com>
2023-05-04 13:27:27 +0100

    feature/adresse : fix affichage adresses sur la page adresse

--- routes/admin/admin.adresse.js
+++ routes/admin/admin.adresse.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { Adresse } = require("../../models");
+const { Adresse, Client } = require("../../models");
 const { PAGINATION_LIMIT_ADMIN } = require("../../helpers/utils_const");
 const check_admin_paginate_value = require("../../helpers/check_admin_paginate_value");
+
 router.get("/", async (req, res) => {
   let { page, start, end } = check_admin_paginate_value(req);
   try {
@@ -28,9 +29,70 @@ router.get("/", async (req, res) => {
   }
 });
 
-// post
+
 router.get("/add", async (req, res) => {
   res.render("adresses/ajoutAdresse")
+})
+
+router.post("/add", async (req, res)=>{
+  res.locals.titre = "nouvelle adresse"
+  let error, success
+
+  const {
+    cli_mail,  
+    adr_structure,
+    adr_nom,
+    adr_prenom,
+    adr_societe,
+    adr_adresse,
+    adr_comp,
+    adr_cp,
+    adr_ville,
+    adr_num_tva,
+    adr_phone,
+    adr_pays} = req.body
+
+    const chekInput = (input) => {
+      return input !== "" ? true : false;
+    };
+
+    if (
+      chekInput(cli_mail) &&
+      chekInput(adr_nom) &&
+      chekInput(adr_prenom) &&
+      chekInput(adr_adresse) &&
+      chekInput(adr_cp) &&
+      chekInput(adr_ville) &&
+      chekInput(adr_pays)
+    ) {
+      try {
+        let cli_id_from_mail = Client.findOne({
+          where: {
+            cli_mail,
+          },
+        });
+
+        let adresse = Adresse.create({
+          ...req.body,
+          cli_id: cli_id_from_mail,
+        });
+
+        if (adresse) {
+          success = "Adresse ajouté!";
+          return res.render("adresses/ajoutAdresse", { success });
+        }
+      } catch (err) {
+        error = "Erreur interne du serveur";
+        return res.render("adresses/ajoutAdresse", {
+          error,
+        });
+      }
+    } else {
+      error = "Veillez remplir tout les champs obligatoire";
+      return res.render("adresses/ajoutAdresse", {
+        error,
+      });
+    }
 })
 
 module.exports = router;
