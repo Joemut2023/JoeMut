57fc9a76ed7619d6b7add3e6b976f094d6630c5a
Romain kabasi <kabasiromain@gmail.com>
2023-05-05 17:46:30 +0100

    feature/admin-promo : remove a promo

--- routes/admin/admin.promo.js
+++ routes/admin/admin.promo.js
@@ -28,12 +28,41 @@ router.get("/add", async (req, res) => {
   }
 });
 router.post("/add", async (req, res) => {
-  console.log(req.body);
+  const { prm_code, prm_pourcent, prm_valeur, prm_debut, prm_fin, prm_actif } =
+    req.body;
   try {
-    // const promos = await Promo.findAll();
-    res.render("promo/add");
+    const oldPromo = await Promo.findOne({ where: { prm_code } });
+    if (oldPromo) {
+      const errorMsg = "Cette promo existe déjà";
+      return res.render("promo/add", {
+        errorMsg,
+      });
+    }
+    const promos = await Promo.create({
+      prm_code,
+      prm_pourcent,
+      prm_valeur,
+      prm_debut,
+      prm_fin,
+      prm_actif,
+    });
+    const succesMsg = "promo créée et enregistrée avec succès";
+    res.render("promo/add", { succesMsg });
   } catch (error) {
     res.status(500).render("promo/add", {
+      error: true,
+      errorMsg: "une erreur est survenue ",
+    });
+  }
+});
+router.post("/delete", async (req, res) => {
+  const prm_id = req.body.prm_id;
+  console.log(prm_id, "prm");
+  try {
+    await Promo.destroy({ where: { prm_id } });
+    res.redirect("/admin/promo");
+  } catch (error) {
+    res.status(500).render("promo/index", {
       error: true,
       errorMsg: "une erreur est survenue ",
     });
