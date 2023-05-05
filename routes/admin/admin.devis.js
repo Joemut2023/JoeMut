fea038d42b1d8b33244815ed25074f79c6128399
seleshabani <seleshabani4@gmail.com>
2023-05-05 14:16:26 +0100

    feature/commande-view : affichage dynamique des adresse

--- routes/admin/admin.devis.js
+++ routes/admin/admin.devis.js
@@ -133,78 +133,99 @@ router.get('/:panier',async (req,res)=>{
 
 router.get('/view/:commandeId',async (req,res)=>{
   const {commandeId} = req.params;
-  let commande = await Commande.findOne({
-    where:{
-      com_id:commandeId
-    },
-    attributes: [
-      "com_id",
-      "com_date",
-      "com_debut_spectacle",
-      "com_fin_spectacle",
-      "pan_id",
-      "frp_id",
-      "com_adr_liv",
-      "com_adr_fac",
-    ],
-    include: [
-      {
-        model: Client,
-        include:[
-          {
-            model:Titre
-          }
-        ]
+  try {
+    let commande = await Commande.findOne({
+      where:{
+        com_id:commandeId
       },
-      {
-        model: Frais_port,
-        include :[
-          {
-            model:Mode_liv_essayage
-          },
-          {
-            model:Mode_liv_spectacle
-          }
-        ]
-      },
-      {
-        model: Panier,
-        include: [
-          {
-            model: Panier_detail,
-            include:[{
-              model:Produit,
-              include:[
-              {
-                model:Media
-              },
-              {
-                model:Quantite
-              },
-              {
-                model:Apply,
+      attributes: [
+        "com_id",
+        "com_date",
+        "com_debut_spectacle",
+        "com_fin_spectacle",
+        "pan_id",
+        "frp_id",
+        "com_adr_liv",
+        "com_adr_fac",
+      ],
+      include: [
+        {
+          model: Client,
+          include:[
+            {
+              model:Titre
+            }
+          ]
+        },
+        {
+          model: Frais_port,
+          include :[
+            {
+              model:Mode_liv_essayage
+            },
+            {
+              model:Mode_liv_spectacle
+            }
+          ]
+        },
+        {
+          model: Panier,
+          include: [
+            {
+              model: Panier_detail,
+              include:[{
+                model:Produit,
                 include:[
-                  {
-                    model:Promo
-                  }
-                ]
-              }
-            ]
-            }]
-          },
-        ],
-      },
-      {
-        model:Essayage
-      }
-    ]
-  });
-  console.log(commande.Essayages);
-  res.render("devis/view",{
-    commande
-  });
+                {
+                  model:Media
+                },
+                {
+                  model:Quantite
+                },
+                {
+                  model:Apply,
+                  include:[
+                    {
+                      model:Promo
+                    }
+                  ]
+                }
+              ]
+              }]
+            },
+          ],
+        },
+        {
+          model:Essayage
+        }
+      ]
+    });
+    let adresse_livraion = await Adresse.findOne({
+      adr_id:commande.com_adr_liv
+    });
+    let adresse_facturation = await Adresse.findOne({
+      adr_id:commande.com_adr_fac,
+      // include:[{
+      //   model:Client
+      // }]
+    });
+    console.log(adresse_facturation.Client);
+    res.render("devis/view",{
+      commande,
+      adresse_livraion,
+      adresse_facturation
+    });
+  } catch (error) {
+    res.render("devis/view",{
+      error:true
+    });
+  }
+  
 })
 
+/**
+ * ajout date essayage
+ */
 router.post('/commande/add-essayage',async (req,res)=>{
   const {com_id,ess_repetition} = req.body;
   try {
