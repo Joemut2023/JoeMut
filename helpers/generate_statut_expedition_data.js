2631afed6b648268da0611d6cc279c8134d51226
Romain kabasi <kabasiromain@gmail.com>
2023-05-05 11:26:21 +0100

    feature/data-seed : create seeder for statut expedition

---
+++ helpers/generate_statut_expedition_data.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const csv = require("csv-parser");
+const path = require("path");
+var results = [];
+const db = require("../models");
+const { Statut_expedition } = require("../models");
+fs.createReadStream(path.resolve(__dirname, "../datas/statut_expedition.csv"))
+  .pipe(csv({ separator: "\n", headers: ["statut"] }))
+  .on("data", (data) => {
+    results.push(data);
+  })
+  .on("end", () => {
+    //results.shift();
+    results.map(async (data) => {
+      await Statut_expedition.create({
+        ste_libelle: data.statut.split(";")[1],
+        ste_actif: parseInt(data.statut.split(";")[2]),
+      });
+    });
+    // console.log(results[0].titre.split(";"));
+  });
