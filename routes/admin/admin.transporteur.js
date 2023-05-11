f9851a501a7da44551c2df7c2162617cc7df45eb
seleshabani <seleshabani4@gmail.com>
2023-05-11 21:32:24 +0100

    feature/transporteur

--- routes/admin/admin.transporteur.js
+++ routes/admin/admin.transporteur.js
@@ -4,4 +4,7 @@ const router = express.Router();
 router.get('/',async (req,res)=>{
     res.render('transporteur/index');
 });
+router.get('/add', (req,res)=>{
+    res.render('transporteur/add');
+})
 module.exports = router;
