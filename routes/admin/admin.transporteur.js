edd55e20b9739c50f6e0985d7c9b9a7be2a14095
seleshabani <seleshabani4@gmail.com>
2023-05-11 21:39:02 +0100

    feature/transporteur : ajout d'un transporteur

--- routes/admin/admin.transporteur.js
+++ routes/admin/admin.transporteur.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
-
+const {Transporteur} = require('../../models');
 router.get('/',async (req,res)=>{
     res.render('transporteur/index');
 });
 router.get('/add', (req,res)=>{
     res.render('transporteur/add');
+});
+router.post('/add',async (req,res)=>{
+    const {trs_libelle} = req.body;
+    try {
+        await Transporteur.create({
+            trs_libelle
+        });
+        res.render('transporteur/add',{
+            notify:'Transporteur ajouté'
+        });
+    } catch (error) {
+        
+    }
 })
 module.exports = router;
