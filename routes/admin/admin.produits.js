e828b5100b3d9641bf67e8362280dcae7ba897f0
pathymavuba <mavubapathy@gmail.com>
2023-05-11 12:16:16 +0100

    feature/editProduit:update tarif  of product

--- routes/admin/admin.produits.js
+++ routes/admin/admin.produits.js
@@ -291,7 +291,30 @@ router.put("/:id", async (req, res) => {
       pro_comment,
       pro_statut,
     },{where:{pro_id:req.params.id}});
-    return res.status(201).json({ product });
+    return res.status(200).json({ product });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+
+router.put("/tarif/:id", async function (req, res) {
+  const { tar_ht, tar_ttc } = req.body;
+  try {
+    const produit = await Produit.findOne({ where: { pro_id: req.params.id } });
+
+    const tarif = await Tarif.create(
+      {
+        tar_debut: new Date(new Date().setDate(new Date().getDate())),
+        tar_fin: null,
+        tar_ht,
+        tar_ttc,
+        tar_statut: 1,
+      },
+      { where: { pro_id: produit.pro_id } }
+    );
+
+    return res.status(200).json(tarif);
   } catch (error) {
     console.log(error.message);
   }
