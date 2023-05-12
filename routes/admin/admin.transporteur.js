ec0b6dfcaee3bb13526fb105da1aa940f7909528
Romain kabasi <kabasiromain@gmail.com>
2023-05-12 12:26:05 +0100

    feature/admni/transporteur : update transporteur

--- routes/admin/admin.transporteur.js
+++ routes/admin/admin.transporteur.js
@@ -39,6 +39,55 @@ router.post("/add", async (req, res) => {
     });
   }
 });
+router.get("/update", async (req, res) => {
+  const { trs_id } = req.query;
+  try {
+    const transporteurs = await Transporteur.findOne({
+      where: { trs_id },
+    });
+
+    res.render("transporteur/update", {
+      transporteurs,
+    });
+  } catch (error) {
+    res.status(500).render("transporteur/add", {
+      error: true,
+      errorMsg: "une erreur est survenue ",
+    });
+  }
+});
+router.post("/update", async (req, res) => {
+  const { trs_libelle, trs_id } = req.body;
+  try {
+    const newTransporteur = await Transporteur.update(
+      {
+        trs_libelle,
+      },
+      { where: { trs_id } }
+    );
+    const transporteurs = await Transporteur.findOne({
+      where: { trs_id },
+    });
+    if (newTransporteur[0] == 1) {
+      const succesMsg =
+        "les informations sur le transporteur mises à jour succès";
+      return res.render("transporteur/update", {
+        succesMsg,
+        transporteurs,
+      });
+    }
+    const nothingMsg = "Aucune nouvelle information trouvée";
+    return res.render("transporteur/update", {
+      nothingMsg,
+      transporteurs,
+    });
+  } catch (error) {
+    res.status(500).render("transporteur/add", {
+      error: true,
+      errorMsg: "une erreur est survenue ",
+    });
+  }
+});
 router.post("/delete", async (req, res) => {
   const { trs_id } = req.body;
   try {
