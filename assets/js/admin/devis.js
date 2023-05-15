7ed10c016db24866b66e8a39aaffaada487dcf9e
seleshabani <seleshabani4@gmail.com>
2023-05-15 17:13:29 +0100

    feature/commande-view : liste des etats

--- assets/js/admin/devis.js
+++ assets/js/admin/devis.js
@@ -1,6 +1,6 @@
-const modalDevis = new bootstrap.Modal(document.querySelector('#modal-devis'), {});
-let allModalToggler = document.querySelectorAll('.td-modal-container');
-const modalDevisBody = document.querySelector('#modal-devis .modal-body');
+//const modalDevis = new bootstrap.Modal(document.querySelector('#modal-devis'), {});
+//let allModalToggler = document.querySelectorAll('.td-modal-container');
+//const modalDevisBody = document.querySelector('#modal-devis .modal-body');
 const accordionsTriggers = document.querySelectorAll('.tr-id');
 
 accordionsTriggers.forEach(accordionTrigger=>{
@@ -15,6 +15,7 @@ accordionsTriggers.forEach(accordionTrigger=>{
             tdBodyHtml += `
                 <div class="produit-detail">
                     <p>${pad.Produit.pro_libelle}</p>
+                    <p>Quantité : ${pad.pad_qte}</p>
                 </div>
             `
         })
@@ -22,29 +23,5 @@ accordionsTriggers.forEach(accordionTrigger=>{
         accordionElt.style.display === ''? accordionElt.style.display = 'table-row':accordionElt.style.display = ''; 
        // console.log(accordionElt.style.display);
     })
-})
-allModalToggler.forEach(modalToggler=>{
-    modalToggler.addEventListener('click',async (e)=>{
-        let commandeId = parseInt(e.target.dataset.commande);
-        let panierId = parseInt(e.target.dataset.panier);
-        let produits = await axios(`${SITE_URL}/admin/devis/${panierId}`);
-        let modalDevisHtml = ``;
-        await produits.data.forEach(pad=>{
-            modalDevisHtml += /*html*/`
-                <div class="modalDevis-item">
-                    <div class="modalDevis-img">
-                        <img src="/images/produits/${pad.Produit.Media[0].med_ressource}" alt=""/>
-                    </div>
-                    <div class="modalDevis-txt">
-                        <h2>${pad.Produit.pro_libelle}</h2>
-                        <p>Quantité : ${pad.pad_qte}</p>
-                    </div>
-                </div>
-            `
-        });
-        modalDevisBody.innerHTML = modalDevisHtml;
-        modalDevis.show();
-    });
 });
-
 
