cf2f69b374d036659815a95d66bb9d0bfcabe4b5
seleshabani <seleshabani4@gmail.com>
2023-05-16 18:45:09 +0100

    feature/produits-location : style pour tableau des listes des produits en location

--- routes/admin/admin.produits.location.js
+++ routes/admin/admin.produits.location.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
+const {Detail_expedition,Retour,Produit,Commande} = require('../../models');
 router.get('/',async (req,res)=>{
-    res.render('produits.location/index');
+    
+    try {
+        res.render('produits.location/index');
+    } catch (error) {
+        res.render('produits.location/index',{
+            error
+        });
+    }
 })
 
 module.exports = router;
