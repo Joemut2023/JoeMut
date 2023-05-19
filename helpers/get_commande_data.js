212e3cdb68dc0613d3bd3d078796615ad3d2535c
Romain kabasi <kabasiromain@gmail.com>
2023-05-19 16:36:41 +0100

    feature/dynam-bon_essaye : create route

--- helpers/get_commande_data.js
+++ helpers/get_commande_data.js
@@ -1,123 +1,123 @@
 const {
-    Panier_detail,
-    Produit,
-    Client,
-    Commande,
-    Panier,
-    Frais_port,
-    Media,
-    Tarif,
-    Adresse,
-    Frais_supp,
-    Autre_frais,
-    Essayage,
-    Quantite
-  } = require("../models");
+  Panier_detail,
+  Produit,
+  Client,
+  Commande,
+  Panier,
+  Frais_port,
+  Media,
+  Tarif,
+  Adresse,
+  Frais_supp,
+  Autre_frais,
+  Essayage,
+  Quantite,
+} = require("../models");
 const { TVA } = require("../helpers/utils_const");
-module.exports = async (commandeId,callback)=>{
+module.exports = async (commandeId, callback) => {
   let quantiteOfEachProduct = [];
-    try {
-      const allproducts = await Produit.findAll();
-        let commande = await Commande.findByPk(commandeId, {
-          include: [
-            { model: Client, attributes: ["cli_mail", "cli_nom"] },
-            { model: Frais_port },
-          ],
-        });
-        let adresseLiv = await Adresse.findOne({
-          where: { adr_id: commande.com_adr_liv },
-        });
-        let adresseFac = await Adresse.findOne({
-          where: { adr_id: commande.com_adr_fac },
-        });
-        let essayage = await Essayage.findAll({where:{com_id:commande.com_id}});
-        let modeLivraison = await Frais_port.findOne({where:{frp_id:commande.frp_id}})
-        
+  try {
+    const allproducts = await Produit.findAll();
+    let commande = await Commande.findByPk(commandeId, {
+      include: [
+        { model: Client, attributes: ["cli_mail", "cli_nom"] },
+        { model: Frais_port },
+      ],
+    });
+    let adresseLiv = await Adresse.findOne({
+      where: { adr_id: commande.com_adr_liv },
+    });
+    let adresseFac = await Adresse.findOne({
+      where: { adr_id: commande.com_adr_fac },
+    });
+    let essayage = await Essayage.findAll({
+      where: { com_id: commande.com_id },
+    });
+    let modeLivraison = await Frais_port.findOne({
+      where: { frp_id: commande.frp_id },
+    });
 
-        //create ref for a command
-        const refCommande = commande.com_num;
-         
-        const panierDetails = await Panier_detail.findAll({
-          include: [
-            {
-              model: Produit,
-              include: [
-                { model: Media, attributes: ["med_id", "med_ressource"] },
-                { model: Tarif},
-              ],
-            },
-          ],
-          where: {
-            pan_id: commande.pan_id,
-          },
-        });
+    //create ref for a command
+    const refCommande = commande.com_num;
 
-        for (let index = 0; index < allproducts.length; index++) {
-          const quantiteInitial = await Quantite.sum("qua_nbre", {
-            where: {
-              pro_id: allproducts[index].pro_id,
-            },
-          });
-          quantiteOfEachProduct.push({
-            id: allproducts[index].pro_id,
-            qty: quantiteInitial,
-          });
-        }
-
-        const produitsPopulaires = await Produit.findAll({
-          limit: 16,
+    const panierDetails = await Panier_detail.findAll({
+      include: [
+        {
+          model: Produit,
           include: [
             { model: Media, attributes: ["med_id", "med_ressource"] },
-            { model: Tarif, attributes: ["tar_ttc"] },
+            { model: Tarif },
           ],
-          where: {
-            pro_en_avant: 1,
-          },
-        });
-          
-        let sous_totalCmd = 0 ;
-        let sous_total = 0;
-        let totalTTC = 0;
-        let totalHT = 0 
-        
-        for (let index = 0; index < panierDetails.length; index++) {
-          sous_totalCmd += panierDetails[index].pad_ttc * panierDetails[index].pad_qte;
-          sous_total += panierDetails[index].pad_ht * panierDetails[index].pad_qte;
-        }
-        
-        let taxe = sous_total *  TVA;
-        
-        let totalCmd = sous_total + commande.Frais_port.frp_ttc + commande.com_frais;
-        // let total = sous_total + commande.Frais_port.frp_ht + commande.com_frais;
-        // res.json({ panierDetails });
-      
-        let livraison = commande.Frais_port.frp_ht?commande.Frais_port.frp_ht:0 
-        totalTTC =
-          sous_total +
-          commande.com_remise +
-          commande.com_frais +
-          taxe +
-          livraison ;
-          totalHT = sous_total + livraison + commande.com_frais
-          return callback(
-            commande,
-            adresseLiv,
-            adresseFac,
-            panierDetails,
-            essayage,
-            modeLivraison,
-            sous_total,
-            taxe,
-            totalTTC,
-            totalHT,
-            sous_totalCmd,
-            produitsPopulaires,
-            totalCmd,
-            quantiteOfEachProduct,
-            refCommande
-          );
+        },
+      ],
+      where: {
+        pan_id: commande.pan_id,
+      },
+    });
 
-    }catch (error) {
-        console.log(error);
+    for (let index = 0; index < allproducts.length; index++) {
+      const quantiteInitial = await Quantite.sum("qua_nbre", {
+        where: {
+          pro_id: allproducts[index].pro_id,
+        },
+      });
+      quantiteOfEachProduct.push({
+        id: allproducts[index].pro_id,
+        qty: quantiteInitial,
+      });
     }
-}
+
+    const produitsPopulaires = await Produit.findAll({
+      limit: 16,
+      include: [
+        { model: Media, attributes: ["med_id", "med_ressource"] },
+        { model: Tarif, attributes: ["tar_ttc"] },
+      ],
+      where: {
+        pro_en_avant: 1,
+      },
+    });
+
+    let sous_totalCmd = 0;
+    let sous_total = 0;
+    let totalTTC = 0;
+    let totalHT = 0;
+
+    for (let index = 0; index < panierDetails.length; index++) {
+      sous_totalCmd +=
+        panierDetails[index].pad_ttc * panierDetails[index].pad_qte;
+      sous_total += panierDetails[index].pad_ht * panierDetails[index].pad_qte;
+    }
+
+    let taxe = sous_total * TVA;
+
+    let totalCmd =
+      sous_total + commande.Frais_port.frp_ttc + commande.com_frais;
+    // let total = sous_total + commande.Frais_port.frp_ht + commande.com_frais;
+    // res.json({ panierDetails });
+
+    let livraison = commande.Frais_port.frp_ht ? commande.Frais_port.frp_ht : 0;
+    totalTTC =
+      sous_total + commande.com_remise + commande.com_frais + taxe + livraison;
+    totalHT = sous_total + livraison + commande.com_frais;
+    return callback(
+      commande,
+      adresseLiv,
+      adresseFac,
+      panierDetails,
+      essayage,
+      modeLivraison,
+      sous_total,
+      taxe,
+      totalTTC,
+      totalHT,
+      sous_totalCmd,
+      produitsPopulaires,
+      totalCmd,
+      quantiteOfEachProduct,
+      refCommande
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
--- routes/admin/admin.router.js
+++ routes/admin/admin.router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Document } = require("../../models");
 const router = express.Router();
 const loginRouter = require("./admin.connexion");
 const devisRouter = require("./admin.devis");
@@ -8,7 +9,7 @@ const fraisPortRouter = require("./admin.fraisport");
 const dashboardRouter = require("./admin.dashboard");
 const adminIsAuth = require("../../middleware/admin/admin.auth");
 const adminLayout = require("../../middleware/admin/admin.layout");
-const FlashMidleware= require("../../middleware/admin/admin.flash");
+const FlashMidleware = require("../../middleware/admin/admin.flash");
 const panierRouter = require("./admin.panier");
 const taillesRouter = require("./admin.taille");
 const couleurRouter = require("./admin.couleurs");
@@ -18,20 +19,28 @@ const clientRouter = require("./admin.client");
 const promoRouter = require("./admin.promo");
 const applyRouter = require("./admin.apply");
 const transporteurRouter = require("./admin.transporteur");
-const categoriesRouter = require("./admin.categories")
-const produitLocationRouter = require("./admin.produits.location")
-const documentRouter = require("./admin.document")
-const ejs = require('ejs');
-const path = require('path');
+const categoriesRouter = require("./admin.categories");
+const produitLocationRouter = require("./admin.produits.location");
+const documentRouter = require("./admin.document");
+const ejs = require("ejs");
+const path = require("path");
 
 router.use("/login", loginRouter);
-router.use("/devis", [adminLayout, adminIsAuth,FlashMidleware], devisRouter);
-router.use("/produits", [adminLayout, adminIsAuth,FlashMidleware], produitsRouter);
+router.use("/devis", [adminLayout, adminIsAuth, FlashMidleware], devisRouter);
+router.use(
+  "/produits",
+  [adminLayout, adminIsAuth, FlashMidleware],
+  produitsRouter
+);
 router.use("/dashboard", [adminLayout, adminIsAuth], dashboardRouter);
 router.use("/paniers", [adminLayout, adminIsAuth], panierRouter);
 router.use("/adresse", [adminLayout, adminIsAuth], adresseRouter);
 router.use("/categories", [adminLayout, adminIsAuth], categoriesRouter);
-router.use("/type-categories", [adminLayout, adminIsAuth], typeCategoriesRouter);
+router.use(
+  "/type-categories",
+  [adminLayout, adminIsAuth],
+  typeCategoriesRouter
+);
 router.use("/dashboard", [adminLayout, adminIsAuth], dashboardRouter);
 router.use("/tailles", [adminLayout, adminIsAuth], taillesRouter);
 router.use("/couleurs/", [adminLayout, adminIsAuth], couleurRouter);
@@ -41,20 +50,32 @@ router.use("/clients", [adminLayout, adminIsAuth], clientRouter);
 router.use("/promo", [adminLayout, adminIsAuth], promoRouter);
 router.use("/apply", [adminLayout, adminIsAuth], applyRouter);
 router.use("/transporteurs", [adminLayout, adminIsAuth], transporteurRouter);
-router.use("/produits-en-location", [adminLayout, adminIsAuth],produitLocationRouter);
-router.use("/documents", [adminLayout, adminIsAuth,FlashMidleware],documentRouter);
-router.get('/bon-essayage/:doc_id',async (req,res)=>{
-    const {doc_id} = req.params;
-    try {
-        let view = await ejs.renderFile(path.join(__dirname, "../../mailTemplate/essayage.ejs"));
-        return res.send(view);
-    } catch (error) {
-       // console.log(error);
-        console.log("erreur rendue bon essayage");
-    }
+router.use(
+  "/produits-en-location",
+  [adminLayout, adminIsAuth],
+  produitLocationRouter
+);
+router.use(
+  "/documents",
+  [adminLayout, adminIsAuth, FlashMidleware],
+  documentRouter
+);
+router.get("/bon-essayage/:doc_id", async (req, res) => {
+  const { doc_id } = req.params;
+  try {
+    let document = await Document.findOne({ where: { doc_id } });
+    let view = await ejs.renderFile(
+      path.join(__dirname, "../../mailTemplate/essayage.ejs"),
+      { document }
+    );
+    return res.send(view);
+  } catch (error) {
+    // console.log(error);
+    console.log("erreur rendue bon essayage");
+  }
 });
-router.post('/delete-flash',async (req,res)=>{
-    delete req.session.flash;
-    res.send('message flash supprimé');
+router.post("/delete-flash", async (req, res) => {
+  delete req.session.flash;
+  res.send("message flash supprimé");
 });
 module.exports = router;
