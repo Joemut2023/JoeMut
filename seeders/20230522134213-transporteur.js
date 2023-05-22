4c933242012eec3dda447ee468b5b4d35f818a48
seleshabani <seleshabani4@gmail.com>
2023-05-22 14:46:04 +0100

    feature/bon-livraison : seeder transporteurs

---
+++ seeders/20230522134213-transporteur.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('Transporteurs',[
+    {
+      trs_libelle:"Chronopost"
+    },
+    {
+      trs_libelle:"Mazda"
+    },
+    {
+      trs_libelle:"Mazda"
+    },
+    {
+      trs_libelle:"Fedex"
+    }
+   ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
