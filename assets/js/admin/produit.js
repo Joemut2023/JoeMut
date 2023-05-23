b9f92a21cc7699a394ef70f14a873b4295cdda01
pathymavuba <mavubapathy@gmail.com>
2023-05-23 14:59:58 +0100

    feature/activeproduit:dynamisation de check_statut

--- assets/js/admin/produit.js
+++ assets/js/admin/produit.js
@@ -1,17 +1,27 @@
 
-const btn_delete = document.querySelectorAll(".btn-delete");
+const statut = document.querySelectorAll(".pro-statut");
 
-btn_delete.forEach(item => {
-  item.addEventListener("click",async function(){
-    // e.preventDefault();
-    //    const produit = await axios.delete(
-    //      `${SITE_URL}/admin/produits/${item.id}`,
-    //      {
-    //        headers: {
-    //          "X-Requested-With": "XMLHttpRequest",
-    //        },
-    //      }
-    //    );
-    //    console.log(produit)
+statut.forEach((element) => {
+  element.addEventListener("change", async function(){
+    const pro_statut = element.checked ? true : false;
+     const Myproduct = await await axios.get(
+       `${SITE_URL}/admin/produits/one/${element.name}`,
+       {
+         headers: {
+           "X-Requested-With": "XMLHttpRequest",
+         },
+       }
+     );
+     const produit = await axios.put(
+       `${SITE_URL}/admin/produits/${Myproduct.data.pro_id}`,
+       { pro_statut },
+       {
+         headers: {
+           "X-Requested-With": "XMLHttpRequest",
+         },
+       }
+     );
+     window.location.reload();
   })
 });
+
