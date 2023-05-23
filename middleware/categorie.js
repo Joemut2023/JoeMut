d4c5274ee240ccb79586b30ab84eec43b656d420
Romain kabasi <kabasiromain@gmail.com>
2023-05-23 12:05:55 +0100

    hot/fix : order categorie item in header

--- middleware/categorie.js
+++ middleware/categorie.js
@@ -1,25 +1,25 @@
-const {Type_categorie,Categorie} = require('../models');
-const getCategorie = async (req,res,next)=>{
-    res.locals.titre = "Accueil"
-    try {
-        let type_categories = await Type_categorie.findAll({
-            include:[{
-                model:Categorie,
-                attributes:[
-                    'cat_id',
-                    'cat_libelle'
-                ]
-            }]
-        })
-        if (type_categories.length === 0) {
-            type_categories = []
-        }
-        res.locals = {
-            type_categories:type_categories
-        };
-    } catch (error) {
-        console.log(error);
+const { Type_categorie, Categorie } = require("../models");
+const getCategorie = async (req, res, next) => {
+  res.locals.titre = "Accueil";
+  try {
+    let type_categories = await Type_categorie.findAll({
+      include: [
+        {
+          model: Categorie,
+          attributes: ["cat_id", "cat_libelle"],
+        },
+      ],
+      order: [[{ model: Categorie }, "cat_libelle", "ASC"]],
+    });
+    if (type_categories.length === 0) {
+      type_categories = [];
     }
-    next();
-}
+    res.locals = {
+      type_categories: type_categories,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+  next();
+};
 module.exports = getCategorie;
