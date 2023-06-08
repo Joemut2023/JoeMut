85c5be4e6c0e64e1b0646e5d7e99728a3a32554f
Romain kabasi <kabasiromain@gmail.com>
2023-06-08 11:45:21 +0100

    hot/fix : fix promotion product price in catalogue FO

--- routes/catalogue.js
+++ routes/catalogue.js
@@ -7,7 +7,8 @@ const {
   Media,
   Tarif,
   Quantite,
-  sequelize,
+  Apply,
+  Promo,
 } = require("../models");
 const { PAGINATION_LIMIT } = require("../helpers/utils_const");
 const check_paginate_value = require("../helpers/check_paginate_value");
@@ -19,7 +20,7 @@ const Logger = require("../helpers/Logger");
 router.get("/", async function (req, res, next) {
   let orderby = req.query.orderby;
   let { page, start, end } = check_paginate_value(req);
-  let quantiteOfEachProduct = []
+  let quantiteOfEachProduct = [];
   var orderCondition;
   let choix;
   if (orderby === "AàZ") {
@@ -42,17 +43,15 @@ router.get("/", async function (req, res, next) {
   try {
     res.locals.titre = "catalogue";
     const typee_categories = await Type_categorie.findAll({
-      order: [
-        ['tyc_ordre', 'ASC']
-      ]
+      order: [["tyc_ordre", "ASC"]],
     });
     const categories = await Categorie.findAll();
     const allproducts = await Produit.findAll({
-      where:{
-        pro_statut:true
-      }
+      where: {
+        pro_statut: true,
+      },
     });
-    
+
     for (let index = 0; index < allproducts.length; index++) {
       const quantiteInitial = await Quantite.sum("qua_nbre", {
         where: {
@@ -61,7 +60,7 @@ router.get("/", async function (req, res, next) {
       });
       quantiteOfEachProduct.push({
         id: allproducts[index].pro_id,
-        qty:quantiteInitial
+        qty: quantiteInitial,
       });
     }
 
@@ -72,10 +71,24 @@ router.get("/", async function (req, res, next) {
         { model: Media, attributes: ["med_id", "med_ressource"] },
         { model: Tarif, attributes: ["tar_ht", "tar_ttc"] },
       ],
-      where:{pro_statut:true},
+      where: { pro_statut: true },
       order: orderCondition,
     });
-
+    const applies = await Apply.findAll({
+      include: [
+        {
+          model: Promo,
+          attributes: [
+            "prm_code",
+            "prm_pourcent",
+            "prm_valeur",
+            "prm_debut",
+            "prm_fin",
+            "prm_actif",
+          ],
+        },
+      ],
+    });
     // res.json({ quantiteOfEachProduct });
     let nbrPages = Math.ceil(allproducts.length / PAGINATION_LIMIT);
     let next = start > 0 ? page + 1 : null;
@@ -94,6 +107,7 @@ router.get("/", async function (req, res, next) {
       choix: choix,
       orderby,
       quantiteOfEachProduct: quantiteOfEachProduct,
+      applies
     });
   } catch (error) {
     Logger.error(error.stack);
@@ -134,9 +148,9 @@ router.get("/:id", async (req, res) => {
 
   try {
     const allproducts = await Produit.findAll({
-      where:{
-        pro_statut:true
-      }
+      where: {
+        pro_statut: true,
+      },
     });
     const categorie = await Categorie.findByPk(id, {
       offset: start,
@@ -163,7 +177,7 @@ router.get("/:id", async (req, res) => {
     }
 
     const type_categorie = await Type_categorie.findByPk(categorie.tyc_id);
-   
+
     const produits = await Produit.findAll({
       offset: start,
       limit: PAGINATION_LIMIT,
@@ -172,8 +186,23 @@ router.get("/:id", async (req, res) => {
         { model: Tarif, attributes: ["tar_ht", "tar_ttc"] },
         { model: Categorie, where: { cat_id: id } },
       ],
-      where:{pro_statut:true},
+      where: { pro_statut: true },
       order: orderCondition,
+    });
+    const applies = await Apply.findAll({
+      include: [
+        {
+          model: Promo,
+          attributes: [
+            "prm_code",
+            "prm_pourcent",
+            "prm_valeur",
+            "prm_debut",
+            "prm_fin",
+            "prm_actif",
+          ],
+        },
+      ],
     });
     let nbrPages = Math.ceil(categorie.Produits.length / PAGINATION_LIMIT);
     res.locals.titre = categorie.cat_libelle;
@@ -189,9 +218,10 @@ router.get("/:id", async (req, res) => {
       choix: choix,
       orderby: orderby,
       quantiteOfEachProduct,
+      applies
     });
   } catch (error) {
-    Logger.error(error.stack)
+    Logger.error(error.stack);
     res.status(500).render("error/serverError", {
       error: true,
       errorMsg: "Une erreur est survenue!",
@@ -212,7 +242,6 @@ router.get("/type/:id", async (req, res) => {
   let choix, ordre;
   let quantiteOfEachProduct = [];
 
-
   if (orderby === "AàZ") {
     orderCondition = [["pro_libelle", "ASC"]];
     choix = "Nom, A à Z";
@@ -231,9 +260,9 @@ router.get("/type/:id", async (req, res) => {
   }
   try {
     const allproducts = await Produit.findAll({
-      where:{
-        pro_statut:true,
-      }
+      where: {
+        pro_statut: true,
+      },
     });
     const type_categorie = await Type_categorie.findByPk(id, {
       include: {
@@ -256,8 +285,24 @@ router.get("/type/:id", async (req, res) => {
         { model: Tarif, attributes: ["tar_ht", "tar_ttc"] },
         { model: Categorie, where: { tyc_id: id } },
       ],
-      where:{pro_statut:true},
+      where: { pro_statut: true },
       order: orderCondition,
+    });
+
+    const applies = await Apply.findAll({
+      include: [
+        {
+          model: Promo,
+          attributes: [
+            "prm_code",
+            "prm_pourcent",
+            "prm_valeur",
+            "prm_debut",
+            "prm_fin",
+            "prm_actif",
+          ],
+        },
+      ],
     });
 
     for (let index = 0; index < allproducts.length; index++) {
@@ -286,9 +331,10 @@ router.get("/type/:id", async (req, res) => {
       choix: choix,
       orderby: orderby,
       quantiteOfEachProduct,
+      applies
     });
   } catch (error) {
-    Logger.error(error.stack)
+    Logger.error(error.stack);
     res.status(500).render("error/serverError", {
       error: true,
       errorMsg: "Une erreur est survenue!",
